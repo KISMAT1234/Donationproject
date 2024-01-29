448964a359bc48433865b1e13cad9000a8ffa064
KISMAT1234 <kismatbohora123@gmail.com>
2024-01-29 09:23:56 +0545

    log

---
+++ frontreact/src/components/afterlogin/bar/Top.jsx
@@ -0,0 +1,27 @@
+function Topbar() {
+   return(
+    <>
+       <div className="">
+           
+           <div className="">
+              <h1>image</h1>
+           </div>
+
+           <div className="">
+               <h1 className="">DONATE CLOTHES</h1>
+               <h1>SAVE LIFE</h1>
+           </div>
+
+           <div className="">
+              <input type="text" className="" placeholder="Search Items Here"/>
+           </div>
+
+       </div>
+       
+
+
+    </>
+   )
+}
+
+export default Topbar;
