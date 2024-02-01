520baea31686f9b8a52b1d8b6bc867ad41120751
KISMAT1234 <kismatbohora123@gmail.com>
2024-02-01 22:16:14 +0545

    submit

--- frontreact/src/components/afterlogin/Upload.jsx
+++ frontreact/src/components/afterlogin/Upload.jsx
@@ -1,28 +1,36 @@
+import image from '../image/post.jpg'
+
 function Uploadcontent (){
  return (
     <>
-       <div className="border-4 border-red-600 bg-blue-400 h-[80vh] p-5 ">
-        <form>
-          <h1 className="text-3xl">UPLOAD YOUR POST HERE</h1>
-          <input type="file" className="w-[%]"/>
-          <h1>description</h1>
-          <input type="text" className=""/>
-          <h1>location:</h1>
-          <input type="text"/><br></br>
-          <h1>deadline</h1>
-          <input type="date"/>
-          <option class="text-2xl">Category:
-            <select>
-                <option value ='music'>Kitchen</option>
-                <option value ='music'>Clothes</option>
-                <option value ='music'>Shoes</option>
-                <option value ='music'>Accessories</option>
-                <option value ='music'>Garden</option>
-                <option value ='music'>Room</option>
-            </select>
-          </option>
-          <button className="bg-green-700 text-2xl rounded w-[20%] h-[5vh]">Post</button>
-        </form>
+       <div className="  bg-blue-400 h-[100vh] p-5  flex sm:justify-around">
+        <div>
+            <h1 className="text-3xl">UPLOAD YOUR POST HERE</h1>
+            <form>
+              <input type="file" className="w-[%]"/>
+              <h1>description</h1>
+              <input type="text" className=""/>
+              <h1>location:</h1>
+              <input type="text"/><br></br>
+              <h1>deadline</h1>
+              <input type="date"/>
+              <option class="text-2xl">Category:
+                <select>
+                    <option value ='music'>Kitchen</option>
+                    <option value ='music'>Clothes</option>
+                    <option value ='music'>Shoes</option>
+                    <option value ='music'>Accessories</option>
+                    <option value ='music'>Garden</option>
+                    <option value ='music'>Room</option>
+                </select>
+              </option>
+              <button className="bg-green-700 text-2xl rounded w-[20%] h-[5vh]">Post</button>
+            </form>
+        </div>
+        <div className="mt-[100px]">
+        <img src={image} classsName=""  alt="" />
+        </div>
+
        </div>
     </>
  )
