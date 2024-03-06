8e008f2e46857e20068d94a907e4bbba46a5cd91
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-06 17:13:07 +0545

    adding upload router

--- backnode/src/router/mainRouter.js
+++ backnode/src/router/mainRouter.js
@@ -1,9 +1,11 @@
 import userRouter from "./userRouter.js"
+import uploadRouter from "./uploadRouter.js"
 import express from 'express'
 
 const mainRouter= express.Router();
 
 mainRouter.use('/user',userRouter);
+mainRouter.use('/upload',uploadRouter);
 
 export default mainRouter;
 
