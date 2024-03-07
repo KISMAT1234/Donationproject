22077026f5d823907f2a88e2f571f07d40edcac0
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-07 21:20:33 +0545

    adding customized border and icon

--- frontreact/src/components/afterlogin/Mainpage.jsx
+++ frontreact/src/components/afterlogin/Mainpage.jsx
@@ -1,7 +1,9 @@
-import React,{useEffect,useState} from "react";
+import React,{useEffect,useState,useRef} from "react";
 import axiosUrl from "../url/Axiosurl";
 import Topbar from "./bar/Top";
 import Leftbar from "./bar/Leftbar"
+import save from  '../image/save.gif'
+
 
 
 
@@ -10,6 +12,8 @@ function Content(){
 
     const [loading, setLoading] = useState(true);
     const [content,setContent]=useState({});
+
+ 
 
      useEffect(()=>{
       const getContent=async ()=>{
@@ -26,7 +30,9 @@ function Content(){
      },[])
 
 
- 
+    const onSubmit = () =>{
+      alert("added to favourite");
+    }
 
  
     return (
@@ -34,22 +40,24 @@ function Content(){
         <Topbar/>
         <div className="md:flex">
            <Leftbar/>
-           <div>
-             {loading ? (<div>Loading content...</div>) : <div>
+    
+             {loading ? (<div>Loading content...</div>) : <div className="flex flex-wrap">
              {
                 content.map((data)=>(
-                   <div className="bg-green-500">
+                  <div className="px-5 h-max ml-5 py-5 mt-5 shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]">
                    <div>{data.name}</div>
                    <div>{data.address}</div>
                    <div>{data.age}</div>
                    <div>{data.description}</div>
+                   <button onClick={onSubmit} className="mt-5 ">
+                     <img src={save} className="h-[5vh]"/>
+                   </button>
                    </div>
                 ))
              }
              </div>
              }
            
-            </div>
             </div>
 
     </>
