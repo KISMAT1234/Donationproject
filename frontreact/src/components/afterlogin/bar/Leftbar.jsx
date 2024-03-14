fd15bda3827099124da7dcbfe867b391f99d025a
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-14 16:05:00 +0545

    box shadow added

--- frontreact/src/components/afterlogin/bar/Leftbar.jsx
+++ frontreact/src/components/afterlogin/bar/Leftbar.jsx
@@ -8,13 +8,13 @@ const logOut = () => {
 
 
     return <>
-     <div className=" rounded-2xl border-2 md:w-[15%] mt-[20px]  border-red-500 h-[8vh] md:h-[80vh] justify-between flex md:block mx-5">
-        <div className="md:mx-[5%] my-3 md:text-4xl md:mt-5px font-bold text-xl hover:text-blue-900 ">
+     <div className=" rounded-2xl mt-[20px]   h-[8vh] md:h-[80vh] justify-between flex md:block mx-5 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
+        <div className="md:mx-[5%] my-3 md:text-4xl md:mt-5px font-bold text-xl hover:text-blue-900 hover:bg-green-400 hover:rounded-2xl px-2">
            <button className="">
               <Link to ="/Mainpage">Content</Link>
            </button>
         </div>
-            <div className="md:mx-[5%] my-3 md:text-4xl md:mt-5px font-bold text-xl hover:text-blue-900 ">
+            <div className="md:mx-[5%] my-3 md:text-4xl md:mt-5px font-bold text-xl hover:text-blue-900 hover:bg-green-400 hover:rounded-2xl px-2">
              <button className="">
                <Link to ="/Mainpage/favouriteswe">Favourites</Link>
             </button>
@@ -26,12 +26,12 @@ const logOut = () => {
            </button>
         </div>
 
-        <div className="md:mx-[5%] my-3 md:text-4xl md:mt-5px font-bold text-xl hover:text-blue-900 ">
+        <div className="md:mx-[5%] my-3 md:text-4xl md:mt-5px font-bold text-xl hover:text-blue-900 hover:bg-green-400 hover:rounded-2xl px-2">
             <button className="">
                <Link to ="/Mainpage/members-list">Member</Link>
             </button>
         </div>
-        <div className="md:mx-[5%] my-3 md:text-4xl md:mt-5px font-bold text-xl hover:text-blue-900 ">
+        <div className="md:mx-[5%] my-3 md:text-4xl md:mt-5px font-bold text-xl hover:text-blue-900 hover:bg-green-400 hover:rounded-2xl px-2">
             <button className="" onClick={logOut}>
                Logout
             </button>
