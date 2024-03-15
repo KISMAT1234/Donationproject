c80ef8827e7a815cb406634216c8287d02f74d3e
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-15 14:43:28 +0545

    fav added succesfully

--- frontreact/src/components/afterlogin/Favourites.jsx
+++ frontreact/src/components/afterlogin/Favourites.jsx
@@ -1,19 +1,41 @@
-import axiosUrl from "../url/Axiosurl";
+// import axiosUrl from "../url/Axiosurl";
 import Topbar from "./bar/Top";
 import Leftbar from "./bar/Leftbar"
+import { useSelector} from 'react-redux'
 
 const Favourites = () => {
+
+  const data= useSelector((state)=>state.data) || []
+ 
+  console.log(data)
 
 return (
     <>
       <Topbar/>
-      <div>
+      <div className="md:flex">
         <Leftbar/>
-
-      <h1>kismat</h1>
+        <div className=" w-[100%]">
+          {
+            data?
+            // <h1>I have data</h1>
+             data.map((star,index)=>(
+              <div key={index}  className=" flex justify-around bg-slate-300 mx-5 my-5 px-5 py-5 rounded-2xl  shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
+                 <div>{index++}</div>
+                 <div>{star.name}</div>
+                 <div>{star.address}</div>
+                 <div>{star.age}</div>
+                 <div>{star.description}</div>
+                 <div className="w-[30%] md:w-[20%]">
+                   <img src={star.image}/>
+                 </div>
+              </div>
+            ))
+            :<h1>you dont have data</h1>
+          }
+        </div>
       </div>
     </>
 )
 }
 
-export  default Favourites;
+export  default Favourites
