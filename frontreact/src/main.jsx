2097cc1922944ac9a5996304f26e0ec99f8fb9e2
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-15 14:44:27 +0545

    store and provider configure

--- frontreact/src/main.jsx
+++ frontreact/src/main.jsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
 import App from './App.jsx'
 import "./index.css"
+import { store } from './store/store'
+import { Provider } from 'react-redux'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>,
      </BrowserRouter>
 )
