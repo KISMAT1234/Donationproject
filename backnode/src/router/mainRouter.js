5e5a8dd4cdcba960ed92255054758ac14ce8b169
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-17 12:59:33 +0545

    loginRouter initialize

--- backnode/src/router/mainRouter.js
+++ backnode/src/router/mainRouter.js
@@ -1,11 +1,14 @@
 import userRouter from "./userRouter.js"
 import uploadRouter from "./uploadRouter.js"
+import loginRouter from "./loginRouter.js"
 import express from 'express'
 
 const mainRouter= express.Router();
 
 mainRouter.use('/user',userRouter);
 mainRouter.use('/upload',uploadRouter);
+mainRouter.use('/login',loginRouter);
+
 
 export default mainRouter;
 
