66307ab2b01bfda39955a102e4b39c6dbd6afacd
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-18 16:34:16 +0545

    responsive

--- frontreact/src/components/beforelogin/Navbar.jsx
+++ frontreact/src/components/beforelogin/Navbar.jsx
@@ -6,23 +6,22 @@ function Nav(){
   return(
     <>
   <div className="">
-        <div className=" bg-yellow-600 w-[100%] flex justify-around">
+        <div className=" bg-yellow-600 w-[100%] md:flex justify-around">
   
-            <div className="w-[50%]">
-              <h1></h1>
-               <img src={logo} className="w-[60%]  my-[20]"  alt="" />
+            <div className="w-[100px]">
+               <img src={logo} className="w-[100%]  my-[20]"  alt="" />
   
             </div>
 
-            <div className="align-center pt-[25px] mr-10">
+            <div className="align-center md:pt-[25px] mr-10">
               <ul >
                 <li className="flex">
-                   <Link to ="/" className="ml-6 text-stone-100 text-4xl        hover:underline decoration-red-600 font-semibold">Home</Link>
-                   <button to ="/" className="ml-6 text-stone-100       hover:underline decoration-red-600 text-4xl font-semibold">About</button>
-                   <button to ="/achievement" className="ml-6 text-stone-100      hover:underline decoration-red-600 text-4xl font-semibold">Achievement</button>
-                   <button to  className="ml-6 text-stone-100 text-4xl   hover:underline decoration-red-600 font-semibold">event</button>
+                   <Link to ="/" className="ml-6 text-stone-100 md:text-4xl text-xl        hover:underline decoration-red-600 font-semibold">Home</Link>
+                   <button to ="/" className="ml-6 text-stone-100       hover:underline decoration-red-600 md:text-4xl text-xl font-semibold">About</button>
+                   <button to ="/achievement" className="ml-6 text-stone-100      hover:underline decoration-red-600 md:text-4xl text-xl font-semibold">Achievement</button>
+                   <button to  className="ml-6 text-stone-100 md:text-4xl text-xl   hover:underline decoration-red-600 font-semibold">event</button>
                    <Link to="/contact">
-                   <button className="ml-6 text-stone-100 text-4xl hover:underline decoration-red-600 font-semibold">contact</button>
+                   <button className="ml-6 text-stone-100 md:text-4xl text-xl hover:underline decoration-red-600 font-semibold">contact</button>
                    </Link>
                 </li>
               </ul>
@@ -30,10 +29,10 @@ function Nav(){
      
             <div className="w-[10%] flex mr-40">
                 <Link to ="/login-form">
-                     <button className="hover:bg-orange-600 bg-green-900  w-[100%] h-[10vh] text-3xl text-amber-50 ml-10 mt-4 ">Login</button>
+                     <button className="hover:bg-orange-600 bg-green-900 rounded-xl w-[100%] h-[10vh] text-3xl text-amber-50 ml-10 mt-4 ">Login</button>
                  </Link>
                  <Link to="/signup-form">
-                         <button className="hover:bg-orange-600 bg-green-900  w-[70%] h-[10vh] text-3xl text-amber-50 ml-14  mt-4">Signup</button><br></br>
+                         <button className="hover:bg-orange-600 bg-green-900 rounded-xl  w-[70%] h-[10vh] text-3xl text-amber-50 ml-14  mt-4">Signup</button><br></br>
                   </Link>
             </div>
         </div>
