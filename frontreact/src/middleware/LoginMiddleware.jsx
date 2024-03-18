450605a8546910d8d1410ae74d4ab3ae25ef2187
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-18 16:37:09 +0545

    token access through localstorage in a variable

--- frontreact/src/middleware/LoginMiddleware.jsx
+++ frontreact/src/middleware/LoginMiddleware.jsx
@@ -1,9 +1,11 @@
 import {Outlet} from "react-router-dom"
 // import React, {useState,useEffect} from "react";
-// import axiosUrl from "../components/url/Axiosurl";
+import axiosUrl from "../components/url/Axiosurl";
 
 
 function LoginMiddleware(){
+    const token = localStorage.getItem("token");
+    console.log(token);
     // const isLogin = true
     // if(isLogin){
     //     <Outlet/>  
