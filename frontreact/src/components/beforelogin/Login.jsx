00c2c79dfd9e30947e75a7f62c3768f24ae8e9ee
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-18 16:34:06 +0545

    error and token set

--- frontreact/src/components/beforelogin/Login.jsx
+++ frontreact/src/components/beforelogin/Login.jsx
@@ -1,15 +1,26 @@
 
 import Nav from "./Navbar"
 import axiosUrl from "../url/Axiosurl";
-import React from 'react';
+import React,{useState} from 'react';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { Button, Checkbox, Form, Input } from 'antd';
+import login from "../image/login.jpeg"
 const App = () => {
 
   const onFinish = (values) => {
+
+
     console.log('Received values of form: ', values);
     axiosUrl.post("/login",values).then((response)=>{
-      console.log(response.data.token);
+      // console.log(response.data.notfound);
+      if(response.data.notfound){
+        console.log(response.data.notfound)
+        alert(response.data.notfound)
+      }else{
+        localStorage.setItem("token",response.data.token);
+        window.location.href="/Mainpage";
+      }
+
     }).catch((err)=>{
       console.log(err)
     })
@@ -17,57 +28,65 @@ const App = () => {
   return (
     <>
     <Nav/>
-    <Form
-      name="normal_login"
-      className="login-form pr-[60%] pl-10 mt-5"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-    >
-      <Form.Item
-        name="email"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your E-mail!',
-          },
-        ]}
+    <div className="mx-10 md:mx-40  my-5 flex shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px]">
+      <Form
+        name="normal_login"
+        className="w-[70%] login-form  px-10 mt-5"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
       >
-        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your Password!',
-          },
-        ]}
-      >
-        <Input
-          prefix={<LockOutlined className="site-form-item-icon" />}
-          type="password"
-          placeholder="Password"
-        />
-      </Form.Item>
-      <Form.Item>
-        <Form.Item name="remember" valuePropName="checked" noStyle>
-          <Checkbox>Remember me</Checkbox>
+        <Form.Item
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your E-mail!',
+            },
+          ]}
+        >
+
+          <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="E-mail" />
         </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Password!',
+            },
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+  
+          <a className="login-form-forgot" href="">
+            Forgot password
+          </a>
+        </Form.Item>
+  
+        <Form.Item>
+          <Button type="primary" htmlType="submit" className="bg-blue-600 login-form-button">
+            Log in
+          </Button>
+          Or <a href="">register now!</a>
+        </Form.Item>
+      </Form>
 
-        <a className="login-form-forgot" href="">
-          Forgot password
-        </a>
-      </Form.Item>
+      <div className="w-[30%]" >
+        <img src={login} className=" rounded-2xl"/>
+      </div>
 
-      <Form.Item>
-        <Button type="primary" htmlType="submit" className="login-form-button">
-          Log in
-        </Button>
-        Or <a href="">register now!</a>
-      </Form.Item>
-    </Form>
+    </div>
     </>
   );
 };
