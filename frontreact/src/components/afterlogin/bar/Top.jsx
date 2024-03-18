7dacf5c0c034ae8071a0aae6a4335f49590e9213
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-18 16:33:37 +0545

    minor cahnge

--- frontreact/src/components/afterlogin/bar/Top.jsx
+++ frontreact/src/components/afterlogin/bar/Top.jsx
@@ -7,19 +7,19 @@ function Topbar() {
 
    return(
     <>
-       <div className="bg-green-400 h-[10vh] flex sm:flex justify-between ">
+       <div className="rounded-b-2xl bg-green-400 h-[10vh] flex sm:flex justify-between shadow-[0px_10px_1px_rgba(221,_221,_221,_1),_0_10px_20px_rgba(204,_204,_204,_1)]">
            
-           <div className="w-[10%]">
-           <img src={logo} className="w-[100%] my-[px] "  alt="" />
+           <div className="w-[100px]">
+           <img src={logo} className="  w-[100%]"  alt="" />
            </div>
 
-           <div className=" md:font-thin ">
+           <div className="  mt-5 text-[10px] sm:text-[120%] md:text-[150%]">
                <h1 className="">DONATE ANYTHING YOU WANT</h1>
             
            </div>
 
            <div className="">
-              <input type="text" className="mt-2 mr-3 md:mr-10 " placeholder="Search Items Here"/>
+              <input type="text" className="mt-2 mr-3 md:mr-10 rounded-md shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]" placeholder="Search Items Here"/>
               <h1 className="font-mono text-xl">Welcome:</h1>
            </div> 
 
