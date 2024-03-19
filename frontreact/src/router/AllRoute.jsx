b4c473b2e6e46260e8593f17996c6b01a368ca18
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-19 17:01:52 +0545

    middleware set

--- frontreact/src/router/AllRoute.jsx
+++ frontreact/src/router/AllRoute.jsx
@@ -9,14 +9,14 @@ import Signup from "../components/beforelogin/Signup"
 import Login from "../components/beforelogin/Login"
 
 
-// import LoginMiddleware from "../middleware/LoginMiddleware"
+import LoginMiddleware from "../middleware/LoginMiddleware"
 
 // After Login Page
 import Mainpage from "../components/afterlogin/Mainpage";
 import MemberList from "../components/afterlogin/Members"
 import Favourites from "../components/afterlogin/Favourites"
 import Uploadcontent from "../components/afterlogin/Upload"
-import Layout from "../components/afterlogin/bar/Outlet"
+// import Layout from "../components/afterlogin/bar/Outlet"
 
 
 
@@ -26,7 +26,7 @@ function RouterList(){
    return (
       <>
       <Routes>
-         <Route path="/" element={<HomePage/>}/>
+         {/* <Route path="/" element={<HomePage/>}/>
          <Route path="/about-page" element={<AboutPage/>}/>
          <Route path="/achievement" element={<Achievement/>}/>
          <Route path="/event" element={<Event/>}/>
@@ -34,15 +34,17 @@ function RouterList(){
 
 
          <Route path="/signup-form" element={<Signup/>}/>
-         <Route path="/login-form" element={<Login/>}/>
+         <Route path="/login-form" element={<Login/>}/> */}
 
 
 
-         <Route path="/Mainpage" element={<Layout/>}>
+         <Route path="/Mainpage" element={<LoginMiddleware/>}>
              <Route path="/Mainpage" element={<Mainpage/>}/>
              <Route path="members-list" element={<MemberList/>}/>
              <Route path="favourites" element={<Favourites/>}/>
             <Route path="upload" element={<Uploadcontent/>}/>
+           {/* <Route path="/Mainpage" element={<Layout/>}>
+            </Route> */}
          </Route>
        </Routes>
  
