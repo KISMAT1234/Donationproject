5ae275d84d3b3ac34125ef169bbf913afd22a8cd
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-21 16:57:25 +0545

    icon added

--- frontreact/src/components/afterlogin/Mainpage.jsx
+++ frontreact/src/components/afterlogin/Mainpage.jsx
@@ -1,86 +1,159 @@
-import React,{useEffect,useState,useRef} from "react";
-import axiosUrl from "../url/Axiosurl";
-import save from  '../image/save.gif'
-import { useDispatch } from 'react-redux'
-import {Star} from "../../slices/addSlice"
-import {Link} from "react-router-dom";
+// import React,{useEffect,useState,useRef} from "react";
+// import axiosUrl from "../url/Axiosurl";
+// import { useDispatch } from 'react-redux'
+// import {Star} from "../../slices/addSlice"
+// import {Link} from "react-router-dom";
+// import { CiStar } from "react-icons/ci";
 
 
 
-function Content(){
+// function Content(){
   
 
-    const [loading, setLoading] = useState(true);
-    const [content,setContent]=useState({});
-    const dispatch = useDispatch()
+//     const [loading, setLoading] = useState(true);
+//     const [content,setContent]=useState({});
+//     const dispatch = useDispatch()
 
  
 
-     useEffect(()=>{
-      const getContent=async ()=>{
-        axiosUrl.get("/upload").then((response)=>{
-        // console.log(response);
-          setContent(response.data);
-            setLoading(false);
-      }).catch((err)=>{
-          console.log(err);
+//      useEffect(()=>{
+//       const getContent=async ()=>{
+//         axiosUrl.get("/upload").then((response)=>{
+//         // console.log(response);
+//           setContent(response.data);
+//             setLoading(false);
+//       }).catch((err)=>{
+//           console.log(err);
       
-      })
-      }
-      getContent();
-     },[])
+//       })
+//       }
+//       getContent();
+//      },[])
 
 
-    const onSubmit = (content) =>{
-      let data=[];
-      data.push(content);
-      // console.log(data);
-      dispatch(Star(data));
+//     const onSubmit = (e, content) =>{
+//       e.preventDefault();
+//       e.stopPropagation();
+//       let data=[];
+//       data.push(content);
+//       // console.log(data);
+//       dispatch(Star(data));
 
 
-      alert("added to favourite");
-    }
+//       // alert("added to favourite");
+//     }
 
  
-    return (
-        <>
+//     return (
+//         <>
     
     
-             {loading ? (<div>Loading content...</div>) : <div className=" sm:grid sm:grid-cols-2 md:grid-cols-3">
-             {
-                content.map((data,index)=>(
-                  <div key={index}  className="md:w-[90%] h-[70vh] px-5 h-max ml-5 mr-5 py-5 mt-10 rounded-xl shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]">
-                   <div>{data.name}</div>
-                   <div>{data.address}</div>
-                   <div>{data.age}</div>
-                   <div>{data.description}</div>
-                   <div className="">
-                       <img src={data.image} width="100" className="w-[90%]" />
-                    </div>
-                   <div className="mt-5 flex justify-between">
-                      <div>
-                      <Link to={"donate/"+data._id}>
-                         <button className="bg-green-500 h-[45px] text-2xl w-[120%] rounded hover:bg-red-600">
-                            Donate Now
-                         </button>
-                       </Link>
+//              {loading ? (<div>Loading content...</div>) : <div className=" sm:grid sm:grid-cols-2 md:grid-cols-3">
+//              {
+//                 content.map((data,index)=>(
+//                   <div key={index}  className="md:w-[90%] h-[70vh] px-5 h-max ml-5 mr-5 py-5 mt-10 rounded-xl shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]">
+//                    <div>{data.name}</div>
+//                    <div>{data.address}</div>
+//                    <div>{data.age}</div>
+//                    <div>{data.description}</div>
+//                    <div className="">
+//                        <img src={data.image} width="100" className="w-[90%]" />
+//                     </div>
+//                    <div className="mt-5 flex justify-between">
+//                       <div>
+//                       <Link to={"donate/"+data._id}>
+//                          <button className="bg-green-500 h-[45px] text-2xl w-[120%] rounded hover:bg-red-600">
+//                             Donate Now
+//                          </button>
+//                        </Link>
 
-                      </div>
-                      <div>
-                        <button onClick={()=>onSubmit(data)} className="">
-                          <img src={save} className="h-[5vh]"/>
-                        </button>
-                      </div>
-                   </div>
+//                       </div>
+//                       <div>
+//                         <button onClick={(e)=>onSubmit(e,data)}>
+//                         <CiStar className="text-4xl" style={{ fill: 'black', backgroundColor: 'white' }} />
+//                         </button>
+//                       </div>
+//                    </div>
                    
-                   </div>
-                ))
-             }
-             </div>
-             }
+//                    </div>
+//                 ))
+//              }
+//              </div>
+//              }
+//     </>
+//   )
+// } 
+
+// export default Content;
+
+import React, { useEffect, useState } from "react";
+import axiosUrl from "../url/Axiosurl";
+import { useDispatch } from 'react-redux';
+import { Star } from "../../slices/addSlice";
+import { Link } from "react-router-dom";
+import { CiStar } from "react-icons/ci";
+
+function Content() {
+  const [loading, setLoading] = useState(true);
+  const [content, setContent] = useState([]);
+  const [clickedIndex, setClickedIndex] = useState(-1); // Track which icon is clicked
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getContent = async () => {
+      axiosUrl.get("/upload")
+        .then((response) => {
+          setContent(response.data);
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    getContent();
+  }, []);
+
+  const onSubmit = (data, index) => {
+    dispatch(Star([data]));
+    setClickedIndex(index); // Update the clicked index
+  }
+
+  return (
+    <>
+      {loading ? (
+        <div>Loading content...</div>
+      ) : (
+        <div className="sm:grid sm:grid-cols-2 md:grid-cols-3">
+          {content.map((data, index) => (
+            <div key={index} className="md:w-[90%] h-[70vh] px-5 h-max ml-5 mr-5 py-5 mt-10 rounded-xl shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]">
+              <div>{data.name}</div>
+              <div>{data.address}</div>
+              <div>{data.age}</div>
+              <div>{data.description}</div>
+              <div className="">
+                <img src={data.image} width="100" className="w-[90%]" alt="Content" />
+              </div>
+              <div className="mt-5 flex justify-between">
+                <div>
+                  <Link to={"donate/" + data._id}>
+                    <button className="bg-green-500 h-[45px] text-2xl w-[120%] rounded hover:bg-red-600">
+                      Donate Now
+                    </button>
+                  </Link>
+                </div>
+                <div>
+                  <button onClick={() => onSubmit(data, index)}>
+                    <CiStar className="text-4xl" style={{ fill: clickedIndex === index ? 'black' : 'blue', backgroundColor: clickedIndex === index ? 'yellow' : 'transparent' }} />
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </>
   )
-} 
+}
 
 export default Content;
 
