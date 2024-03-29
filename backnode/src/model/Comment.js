052cb3aa157850a068045cafc353819678d3326f
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-29 21:56:40 +0545

    schema written for comment

---
+++ backnode/src/model/Comment.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const commentSchema = new mongoose.Schema({
+    // postId: {
+    //     type:mongoose.Schema.Types.ObjectId,
+    //     ref:'Upload'
+    // },
+    // userId:{
+    //     type:mongoose.Schema.Types.ObjectId,
+    //     ref:'User'
+    // },
+    comment: String,
+});
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+export default Comment;
+
