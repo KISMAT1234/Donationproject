a3f376e4789d08797d08611fb445ce7931d97a1e
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-29 21:57:11 +0545

    routing cmtROuter

--- backnode/src/router/mainRouter.js
+++ backnode/src/router/mainRouter.js
@@ -1,15 +1,15 @@
+import express from 'express'
 import userRouter from "./userRouter.js"
 import uploadRouter from "./uploadRouter.js"
 import loginRouter from "./loginRouter.js"
-
-
-import express from 'express'
+import cmtRouter from "./commentRouter.js";
 
 const mainRouter= express.Router();
 
 mainRouter.use('/user',userRouter);
 mainRouter.use('/upload',uploadRouter);
 mainRouter.use('/login',loginRouter);
+mainRouter.use('/comment',cmtRouter)
 
 
 export default mainRouter;
