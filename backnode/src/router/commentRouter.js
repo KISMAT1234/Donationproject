82d77e5f7a9c50e857e64d44c9889bd6e74b469a
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-29 21:56:56 +0545

    router for comment path

---
+++ backnode/src/router/commentRouter.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import CommentController from '../controller/CommentController.js';
+import authenticate from '../middleware/Authenticate.js';
+
+const cmtRouter = express.Router()
+
+const commentInstance = new CommentController();
+
+cmtRouter.post("/",authenticate, commentInstance.storeComment)
+cmtRouter.get("/",commentInstance.getComment)
+
+export default cmtRouter;
