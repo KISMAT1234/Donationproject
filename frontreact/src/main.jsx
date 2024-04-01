99ac4263b8220ec94dbd02722e8c5cf5f65c558a
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-01 17:20:39 +0545

    minor change

--- frontreact/src/main.jsx
+++ frontreact/src/main.jsx
@@ -11,6 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <Provider store={store}>
           <App />
-        </Provider>,
+        </Provider>
      </BrowserRouter>
 )
