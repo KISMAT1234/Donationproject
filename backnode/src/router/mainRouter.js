b69745714b6eb64131f35a39017a12042018a345
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-01 17:20:13 +0545

    minor change

--- backnode/src/router/mainRouter.js
+++ backnode/src/router/mainRouter.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import userRouter from "./userRouter.js"
-import uploadRouter from "./uploadRouter.js"
+import postRouter from "./postRouter.js"
 import loginRouter from "./loginRouter.js"
 import cmtRouter from "./commentRouter.js";
 
 const mainRouter= express.Router();
 
 mainRouter.use('/user',userRouter);
-mainRouter.use('/upload',uploadRouter);
+mainRouter.use('/upload',postRouter);
 mainRouter.use('/login',loginRouter);
 mainRouter.use('/comment',cmtRouter)
 
