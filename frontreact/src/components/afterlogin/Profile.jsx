735b91a20e47704b15270c6511ab2055f4f13d1f
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-02 16:16:35 +0545

    file added

--- frontreact/src/components/afterlogin/Profile.jsx
+++ frontreact/src/components/afterlogin/Profile.jsx
@@ -5,25 +5,46 @@ import axiosUrl from "../url/Axiosurl";
 const Profile = () => {
 
     const [profile, setProfile] = useState([]);
-  useEffect(()=>{
-     axiosUrl.get("/user").then((response)=>{
-       console.log(response.data.data);
-       setProfile([response.data.data])
-     }).catch((err)=>{
-       console.log(err);
-     })
-},[])
+      const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+  };
+
+  const handleUpload = () => {
+    // You can send the file to the server for storage here
+    console.log('Uploading file:', selectedFile);
+  };
+//   useEffect(()=>{
+//      axiosUrl.get("/user").then((response)=>{
+//        console.log(response.data.data);
+//        setProfile(response.data.data)
+//      }).catch((err)=>{
+//        console.log(err);
+//      })
+// },[])
+
+
 
   return (
     <div className="mx-5 my-10  shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
         <h1>Profile</h1>
-        {
+        {/* {
         profile.map((data, index)=>(
             <div key={index}>
                <div>{data.username}</div>
             </div>
         ))
-        }
+        } */}
+
+
+         <div>
+            <input type="file" onChange={handleFileChange} />
+            <button onClick={handleUpload}>Upload</button>
+          </div>
+
+
     </div>
   )
 }
