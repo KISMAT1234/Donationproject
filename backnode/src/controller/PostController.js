b68bd233b992bd41b6601bc40bbb9e5652962de9
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-07 15:25:55 +0545

    getting user query to fetch data accordingly

--- backnode/src/controller/PostController.js
+++ backnode/src/controller/PostController.js
@@ -26,7 +26,13 @@ class PostController{
 
     async content(req,res){
         try{
-            const uploads =  await Post.find();   // Fetching upload post   
+            let page =Number(req.query.page) || 1;
+            console.log(page,'page value')
+            let size = Number(req.query.size) || 3;
+
+            let skip = (page-1) * size;
+            // console.log(skip,'skip value');
+            const uploads =  await Post.find().skip(skip).limit(size);   // Fetching upload post   
             const uploadsWithUser = await Promise.all(uploads.map(async (upload) => {  // mapping over uploads inside Promise.all  to ensure all user queries are completed before continuing
                 const user = await User.findById(upload.userId); // Fetching  user information  which user is going to post
                 
