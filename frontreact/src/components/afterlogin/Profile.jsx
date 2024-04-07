ef542782f0db8205a9efeb490a399a77c2a55b76
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-07 15:29:01 +0545

    user data fetch successfull

--- frontreact/src/components/afterlogin/Profile.jsx
+++ frontreact/src/components/afterlogin/Profile.jsx
@@ -7,42 +7,35 @@ const Profile = () => {
     const [profile, setProfile] = useState([]);
       const [selectedFile, setSelectedFile] = useState(null);
 
-  const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    setSelectedFile(file);
-  };
-
-  const handleUpload = () => {
-    // You can send the file to the server for storage here
-    console.log('Uploading file:', selectedFile);
-  };
-//   useEffect(()=>{
-//      axiosUrl.get("/user").then((response)=>{
-//        console.log(response.data.data);
-//        setProfile(response.data.data)
-//      }).catch((err)=>{
-//        console.log(err);
-//      })
-// },[])
+  
+  useEffect(()=>{
+     axiosUrl.get("/user").then((response)=>{
+       console.log(response.data.data);
+       setProfile([response.data.data])
+     }).catch((err)=>{
+       console.log(err);
+     })
+},[])
 
 
 
   return (
-    <div className="mx-5 my-10  shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+    <div className="mx-5 my-10  rounded-2xl shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
         <h1>Profile</h1>
-        {/* {
+        {
         profile.map((data, index)=>(
-            <div key={index}>
-               <div>{data.username}</div>
+            <div key={index} className="">
+               <div className="flex justify-center">
+                <img src={data.image}/>
+               </div>
+               <div className="flex justify-center text-6xl font-medium">{data.username}</div>
+               <div className="flex justify-center text-xl">{data.email}</div>
             </div>
         ))
-        } */}
+        }
 
 
-         <div>
-            <input type="file" onChange={handleFileChange} />
-            <button onClick={handleUpload}>Upload</button>
-          </div>
+       
 
 
     </div>
