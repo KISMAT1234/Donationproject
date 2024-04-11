3cbdb56d981905c784533f920194548c74329f3b
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-11 18:22:54 +0545

    userId send to Email to verify

--- backnode/src/helper/emailToken.js
+++ backnode/src/helper/emailToken.js
@@ -1,11 +1,21 @@
 import sendEmail from "./sendEmail.js";
+import Token from "../model/Token.js";
+
 
 class emailToken{
-    static token({email, subject,info}){
-    //   console.log(email, subject,info);
-      const token = info.generateToken();
-    //   console.log(token)
-      sendEmail({to:email,subject:subject,token})
+    static async token({email,userId, subject,info}){
+        //   console.log(email, subject,info)
+        // const userId  = user._id;
+        // console.log(userId,'userid value')
+        const token = await info.generateToken();
+        console.log(token, 'token value')
+        const emailToken = await new Token({
+          userId:userId._id,
+          token:token
+        })
+        // .save()
+        console.log(emailToken,'email token')
+         await sendEmail({to:email,userId,subject:subject,token})
     }
 }
 export default emailToken;
