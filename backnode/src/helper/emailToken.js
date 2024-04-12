8ebc258a02f8513edc66373831fdb7de4f2f9e09
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-12 16:08:17 +0545

    console for data

--- backnode/src/helper/emailToken.js
+++ backnode/src/helper/emailToken.js
@@ -4,17 +4,18 @@ import Token from "../model/Token.js";
 
 class emailToken{
     static async token({email,userId, subject,info}){
-        //   console.log(email, subject,info)
+          // console.log(email,userId, subject,info)
         // const userId  = user._id;
         // console.log(userId,'userid value')
         const token = await info.generateToken();
-        console.log(token, 'token value')
+        // console.log(token, 'token value')
+        
         const emailToken = await new Token({
           userId:userId._id,
           token:token
-        })
-        // .save()
-        console.log(emailToken,'email token')
+        }).save()
+
+        // console.log(emailToken,'email token')
          await sendEmail({to:email,userId,subject:subject,token})
     }
 }
