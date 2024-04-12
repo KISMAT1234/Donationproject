139dcc5655afb696d9587f3e1a94f31d66901104
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-12 16:07:52 +0545

    token added

--- backnode/src/controller/UserController.js
+++ backnode/src/controller/UserController.js
@@ -3,6 +3,7 @@ import ResponseHandler from "../helper/ResponseHandler.js"
 import sendEmail from "../helper/sendEmail.js"
 const responseInstance = new ResponseHandler();
 import emailToken from "../helper/emailToken.js"
+import Token from "../model/Token.js";
 
 
 
@@ -50,7 +51,7 @@ class UserController{
                 const user = new User({...req.body, image:imageName}); 
                 
                 // console.log(req.body);
-                // await user.save();
+                await user.save();
                 // console.log(user)
                 const email = user.email
                 const userId = user._id;
@@ -61,11 +62,45 @@ class UserController{
                 }else{
                     console.log("error");
                 }
-                return res.status(201).json({message:'Signup Successfull'});
+                return res.status(201).json({message:'Email sent to your account for verification'});
             }
             catch(err){
                  return res.status(500).json(err);
             }
+    }
+
+    async verifyEmail(req,res){
+       try{
+         const user_id = req.params.id;
+        //  console.log(user_id,'params userid')
+         const user = await User.findById({_id: user_id});
+        //  console.log(user,'user id')
+         if(!user){
+            return res.status(400).json({message:"Invalid user"});
+         }
+         const token = await Token.findOne({
+            userId: user_id,
+            token:req.params.token
+         })
+        //  console.log(token,'token users data')
+         if(!token){
+            return res.status(400).json({message:"Invalid token"});
+         }
+
+         const update = await User.updateOne({ _id: user_id }, { $set: { isVerified: true } });
+        //  console.log(update,'update successfull user data')
+
+        //  await Token.deleteOne({
+        //     userId: user_id,
+        //     token: req.params.token
+        //   });
+
+         res.status(200).json({message:"Email verified successfully"})
+       }catch(err){
+
+        res.status(500).json({message:"Error in verifying email"})
+
+       }
     }
 
 
