cc6df0e014368e29f5ae8ea36af84567b4ab5771
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-13 13:21:48 +0545

    success and error handler added in if else writing condition for them

--- backnode/src/helper/ResponseHandler.js
+++ backnode/src/helper/ResponseHandler.js
@@ -1,4 +1,14 @@
-class ResponseHandler{
+class Handler{
+
+  responseHandler(res, httpStatusCode,message,data){
+
+     if(httpStatusCode === 200 || httpStatusCode === 201 || httpStatusCode === 202){
+      this.successResponse(res, httpStatusCode,message,data);
+     }else{
+       this.errorResponse(res, httpStatusCode,message,data);
+     }
+  }
+
    successResponse(res,httpStatusCode,message,data){
       const response = {
          success:true,
@@ -8,6 +18,15 @@ class ResponseHandler{
       let statusCode = httpStatusCode || 200
       res.status(statusCode).send(response)
       }
+
+      errorResponse(res,httpStatusCode,message){
+         var response = {
+            success: false,
+            message: message || "Internal Server Error",
+          };
+          let statusCode = httpStatusCode || 500;
+          res.status(statusCode).send(response);
+      }
 }
 
-export default ResponseHandler;
+export default Handler;
