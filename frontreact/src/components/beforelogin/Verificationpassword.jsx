4fbc8557b996e1a304e8f1ae86d632eb7cec283d
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-14 15:52:08 +0545

    getting token and userid through params successfully

--- frontreact/src/components/beforelogin/Verificationpassword.jsx
+++ frontreact/src/components/beforelogin/Verificationpassword.jsx
@@ -5,14 +5,24 @@ import React,{useState} from "react";
 
 const Verificationpassword = () => {
     const [email,setEmail] = useState('');
+    
+    const handleChange = (e) => {
+      setEmail(e.target.value)
+    }
 
     const submitEmail = (e) => {
         e.preventDefault();
+        console.log(email,'email')
+        axiosUrl.post("/login/forgot",{email}).then((response)=>{
+          console.log(response);
+        }).catch((err)=>{
+          console.log(err);
+        })
     }
     return (
         <>
           <form onSubmit={submitEmail}>
-            <input type="email" className="bg-green-400"/>
+            <input type="email" className="bg-green-400" onChange={ handleChange}/>
             <button type="submit">Submit</button>
           </form>
 
