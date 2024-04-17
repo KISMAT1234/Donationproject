d41ea3befaa9b3dae4c1d38a13880812a0bdb703
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-17 21:54:26 +0545

    winston added to see error in log file

--- backnode/src/logger/winston.js
+++
@@ -1,9 +0,0 @@
-import winston from "winston";
-
-const logger = winston.createLogger({
-    level:"warn",
-    format: winston.format.simple(),
-    transports:[new winston.transports.Console()]
-});
-
-logger.error("error from logger")
---
+++ backnode/src/logger/winstonLogger.js
@@ -0,0 +1,51 @@
+import winston,{format} from "winston";
+// import {EnvConfiguration.LOG_LEVEL} from ""
+const {printf, timestamp, combine, colorize, errors, json} = format;
+
+const myFormat = printf(({ level, message, stack, timestamp }) => {
+    return `${timestamp}  ${level}: ${stack || message}`;
+});
+
+const Loglevels = {
+    error: 0,
+    warn: 1,
+    info: 2,
+    http: 3,
+    verbose: 4,
+    debug: 5,
+    silly: 6,
+  }
+
+
+let Logger;
+console.log(process.env.NODE_ENV,'env')
+if (process.env.NODE_ENV == 'production') {
+
+   Logger = winston.createLogger({
+    level:Loglevels,     
+    format: combine(
+        colorize(),
+        timestamp({format: "yyyy-MM-dd HH:mm:ss"}),
+        errors({stack: true}),
+        json(),
+        // myFormat
+    ),   // Text format for logging       
+    transports:[
+        new winston.transports.File({
+            filename:"log/error_log.txt",
+            level:"error",
+        })
+    ]
+});
+}else{
+     Logger = winston.createLogger({
+        label: "warn",
+        transports: [new winston.transports.Console()],
+        format: winston.format.simple(),
+      })
+}
+
+// logger.error("error from logger")
+Logger.warn("warning message");
+
+export default Logger
