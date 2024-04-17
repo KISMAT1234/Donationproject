1b0d60e544edd21d1448f333a698b960ecdfe67a
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-17 22:04:28 +0545

    production and development handler checked

---
+++ backnode/src/logger/errorHandler.js
@@ -0,0 +1,44 @@
+// import Logger from "./winstonLogger";
+
+// const errorHandler = (err, req, res, next) => {
+//     if(process.env.NODE_ENV === 'production'){
+//         developmentErrorHandler(err, req, res, next);
+//     } else {
+//         productionErrorHandler(err, req, res, next);
+//     }
+//  }
+
+//  const developmentErrorHandler = () => {
+
+//  }
+
+//  const productionErrorHandler = (err, req) => {
+
+//  }
+
+
+
+const handleProdError = (err, res) => {
+    err.statusCode = err.statusCode || 500
+    err.message = err.message || 'Opps Something went wrong'
+    var response = { success: false, message: err.message }
+    // Logger.info({ message: err.message })
+    res.status(err.statusCode).send(response)
+  }
+  
+  const handleDevError = (err, res) => {
+    err.statusCode = err.statusCode || 500
+    err.message = err.message || 'Opps Something went wrong'
+    var response = { success: false, message: err.message, stack: err.stack }
+    res.status(err.statusCode).send(response)
+  }
+  
+  const errorHandler = (err, req, res, next) => {
+    if (process.env.NODE_ENV === 'development') {
+      handleDevError(err, res)
+    } else {
+      handleProdError(err, res)
+    }
+  }
+
+  export default errorHandler;
