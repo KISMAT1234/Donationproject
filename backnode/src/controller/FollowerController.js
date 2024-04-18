35d67e87f8dbe6d9260b8d5c5f80f8d3fc90ab13
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-18 18:57:56 +0545

    follower controller accepting userId

--- backnode/src/controller/FollowerController.js
+++ backnode/src/controller/FollowerController.js
@@ -1,65 +1,79 @@
-import User from "../model/Userprofile"
+import User from "../model/Userprofile.js"
 
-class SubscribeController{
-   async  getUserChannelProfile(req,res){
+class FollowController{
+   async  getUserProfile(req,res){
     try{
-     const {username} = req.params
+        const followerId = req.user.userId;
+        const followingId = req.params.id;
+        const user = await new User({follower:followerId, following:followingId});
+     let following = user.following;
+     following.push(user._id);
 
-     const channel = await User.aggregate([
-        {
-            $match:{
-                username: username?.toLowerCase()
-            }
-        },
-        {
-            $lookup:{
-                from: "Follow",
-                localfield:"_id",
-                foreignField:"follower",
-                as:"followers"
-            }
-        },
-        {
-            $lookup:{
-                from: "Follow",
-                localfield:"_id",
-                foreignField:"following",
-                as:"followedTo"
-            }
-        },
-        {
-            $addFields: {
-                followerCount:{
-                    $size:"followers"
-                },
-                followingCount:{
-                    $size:"followedTo"
-                },
-                isFollowed:{
-                    $cond:{
-                        if:{$in: [req.user?._id,"$follower.follower"]}
-                    }
-                }
-            }
-        },
-        {
-            $project:{
-                fullName: 1,
-                username: 1,
-                followerCount: 1,
-                followingCount: 1,
-                isFollowed: 1,
+     const people = await User.find({_id: {$nin:following}})
+
+    //  const {username} = req.params
+
+    //  const channel = await User.aggregate([
+    //     {
+    //         $match:{
+    //             username: username?.toLowerCase()
+    //         }
+    //     },
+    //     {
+    //         $lookup:{
+    //             from: "Follow",
+    //             localfield:"_id",
+    //             foreignField:"follower",
+    //             as:"followers"
+    //         }
+    //     },
+    //     {
+    //         $lookup:{
+    //             from: "Follow",
+    //             localfield:"_id",
+    //             foreignField:"following",
+    //             as:"followedTo"
+    //         }
+    //     },
+    //     {
+    //         $addFields: {
+    //             followerCount:{
+    //                 $size:"followers"
+    //             },
+    //             followingCount:{
+    //                 $size:"followedTo"
+    //             },
+    //             isFollowed:{
+    //                 $cond:{
+    //                     if:{$in: [req.user?._id,"$follower.follower"]}
+    //                 }
+    //             }
+    //         }
+    //     },
+    //     {
+    //         $project:{
+    //             fullName: 1,
+    //             username: 1,
+    //             followerCount: 1,
+    //             followingCount: 1,
+    //             isFollowed: 1,
+    //             email: 1
                 
-            }
-        }
+    //         }
+    //     }
 
-     ])
+    //  ])
 
-     if(!channel?.length){
-        console.log("Channel not found");
-     }
+    //  if(!channel?.length){
+    //     console.log("Channel not found");
+    //  }
+
     }catch(err){
         console.log(err)
     }
+
    }
+
 }
+
+export default FollowController;
