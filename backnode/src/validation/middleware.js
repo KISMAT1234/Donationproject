06352d8f1e137c5d9ae378d1b69778a0c25ea378
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-19 20:23:56 +0545

    middleware for validation

---
+++ backnode/src/validation/middleware.js
@@ -0,0 +1,17 @@
+import Handler from "../logger/ResponseHandler.js"
+const responseInstance = new Handler();
+
+const validate = (schema) => async(req, res, next) => {
+    try{
+        const parseBody = await schema.parseAsync(req.body)
+        req.body = parseBody;
+        next()
+    }catch(err){
+        const errMessage = err.errors[0].message
+        console.log(errMessage,'err message');
+        // console.log(err);
+        return responseInstance.responseHandler(res,500,errMessage)
+    }
+}
+
+export default validate
