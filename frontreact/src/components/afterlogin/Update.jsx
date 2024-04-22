2004c5b6ddf953ebf08444d03efeac0847a59d77
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-22 16:04:57 +0545

    update component for user info

--- frontreact/src/components/afterlogin/Update.jsx
+++ frontreact/src/components/afterlogin/Update.jsx
@@ -1,8 +1,52 @@
-const Update = () => {
-    return (
-        <>
-          <h1>Update profile</h1>
-        </>
-    )
+import React from 'react';
+import { Button, Form, Input } from 'antd';
+
+
+
+
+const MyFormItemContext = React.createContext([]);
+function toArr(str) {
+  return Array.isArray(str) ? str : [str];
 }
+const MyFormItemGroup = ({ prefix, children }) => {
+  const prefixPath = React.useContext(MyFormItemContext);
+  const concatPath = React.useMemo(() => [...prefixPath, ...toArr(prefix)], [prefixPath, prefix]);
+  return <MyFormItemContext.Provider value={concatPath}>{children}</MyFormItemContext.Provider>;
+};
+const MyFormItem = ({ name, ...props }) => {
+  const prefixPath = React.useContext(MyFormItemContext);
+  const concatName = name !== undefined ? [...prefixPath, ...toArr(name)] : undefined;
+  return <Form.Item name={concatName} {...props} />;
+};
+const Update = () => {
+  const onFinish = (value) => {
+    console.log(value);
+  };
+  return (
+    <>
+    <div className="px-5 py-5">
+    <Form name="form_item_path" layout="vertical" onFinish={onFinish}>
+      <MyFormItemGroup prefix={['user']}>
+        <MyFormItemGroup prefix={['name']}>
+          <MyFormItem name="userName" label=" userName">
+            <Input />
+          </MyFormItem>
+          <MyFormItem name="email" label="Email">
+            <Input />
+          </MyFormItem>
+        </MyFormItemGroup>
+
+        <MyFormItem name="age" label="Age">
+          <Input />
+        </MyFormItem>
+      </MyFormItemGroup>
+
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form>
+    </div>
+    </>
+  );
+};
 export default Update;
