4347a854011d14fcde2c4243bcff96fd7cf62100
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-23 13:14:19 +0545

    sending user slug in link

--- frontreact/src/components/afterlogin/Profile.jsx
+++ frontreact/src/components/afterlogin/Profile.jsx
@@ -13,26 +13,29 @@ const Profile = () => {
   
   useEffect(()=>{
      axiosUrl.get("/login/token").then((response)=>{
-       console.log(response.data.userData);
+      //  console.log(response.data.userData);
        setProfile([response.data.userData])
-     }).catch((err)=>{
-       console.log(err);
-     })
-
-    //  axiosUrl.get("/upload").then((response)=>{
-    //   console.log(response.data.data);
+       
+      }).catch((err)=>{
+        console.log(err);
+      })
+      
+      //  axiosUrl.get("/upload").then((response)=>{
+        //   console.log(response.data.data);
     //   setProfile([response.data.data])
     // }).catch((err)=>{
     //   console.log(err);
     // })
-},[])
+  },[])
+  // console.log(profile[0]?.username, "profile value");
+
 
 
 
   return (
     <div className="mx-5 my-10 rounded-2xl shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
       <div className="flex justify-between">
-       <Link to = "/Mainpage/profile/update-profile">
+       <Link to = {"/Mainpage/profile/" + profile[0]?.slug}>
         <button className="bg-red-600  hover:bg-green-600 rounded-xl px-2 py-1 mx-5 my-5 justify-end">
             {/* <h1>Edit</h1> */}
             <FaEdit className="ml-1 text-4xl" />
