9dd1d27e54835da3b4e1e67e2460eb0c2674a64d
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-24 16:57:52 +0545

    error message send to winston logger

--- backnode/src/middleware/errorHandler.js
+++ backnode/src/middleware/errorHandler.js
@@ -1,10 +1,10 @@
 import Logger from "../logger/winstonLogger.js";
 
 const errorHandler = (err, req, res, next) => {
-    if(process.env.NODE_ENV === 'production'){
-        developmentErrorHandler(err, req, res, next);
-    } else {
+    if(process.env.NODE_ENV === 'development'){
         productionErrorHandler(err, req, res, next);
+    } else {
+        developmentErrorHandler(err, req, res, next);
     }
  }
 
@@ -12,42 +12,23 @@ const errorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500
   err.message = err.message || 'Opps Something went wrong'
   var response = { success: false, message: err.message, stack: err.stack }
+  Logger.error({ message: err.message })
   res.status(err.statusCode).send(response)
  }
 
  const productionErrorHandler = (err, req) => {
+    console.log("start from here of error")
+    console.log(err.message,'error in production')
+    Logger.error({ message: err.message })
 
-  err.statusCode = err.statusCode || 500
-  err.message = err.message || 'Opps Something went wrong'
-  var response = { success: false, message: err.message }
-  Logger.info({ message: err.message })
-  res.status(err.statusCode).send(response)
+//   err.statusCode = err.statusCode || 500
+//   err.message = err.message || 'Opps Something went wrong'
+//   var response = { success: false, message: err.message }
+//   Logger.error({ message: err.message })
+//   res.status(err.statusCode).send(response)
 
  }
 
 
-
-// const handleProdError = (err, res) => {
-//     err.statusCode = err.statusCode || 500
-//     err.message = err.message || 'Opps Something went wrong'
-//     var response = { success: false, message: err.message }
-//     // Logger.info({ message: err.message })
-//     res.status(err.statusCode).send(response)
-//   }
-  
-//   const handleDevError = (err, res) => {
-//     err.statusCode = err.statusCode || 500
-//     err.message = err.message || 'Opps Something went wrong'
-//     var response = { success: false, message: err.message, stack: err.stack }
-//     res.status(err.statusCode).send(response)
-//   }
-  
-//   const errorHandler = (err, req, res, next) => {
-//     if (process.env.NODE_ENV === 'development') {
-//       handleDevError(err, res)
-//     } else {
-//       handleProdError(err, res)
-//     }
-//   }
 
   export default errorHandler;
