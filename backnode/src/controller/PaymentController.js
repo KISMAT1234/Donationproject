94d1460d06d1bbcf50c3fcde5a6fd6ca8432c9ac
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-26 16:09:35 +0545

    payment send to frontend

---
+++ backnode/src/controller/PaymentController.js
@@ -0,0 +1,50 @@
+import stripe from 'stripe';
+
+
+
+import dotenv from 'dotenv'
+import Handler from "../logger/ResponseHandler.js"
+
+const secretKey = process.env.SECRET_STRIPE_KEY;
+const stripeInstance = new stripe(secretKey);
+const responseInstance = new Handler();
+
+dotenv.config();
+
+
+
+class PaymentController{
+    async pay(req,res){
+        try{
+
+            const info = req.body
+            console.log(info,'info result');
+
+            const session = await stripeInstance.checkout.sessions.create({
+              payment_method_types:["card"],
+              line_items:[{
+                price_data: {
+                  currency: "usd",
+                  product_data: {
+                    name:"hari"
+                  },
+                  unit_amount: 2000,
+                },
+                quantity: 1,
+              }],
+              mode:"payment",
+              success_url: `${process.env.FRONTEND_URL}/Mainpage/payment-success`,
+              cancel_url: `${process.env.FRONTEND_URL}/Mainpage/payment-cancel`,
+            //   cancel_url:"http://localhost:3000/cancel",
+            })
+
+            return responseInstance.responseHandler(res,200,'payment', session.id)
+            
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+}
+
+export default PaymentController;
