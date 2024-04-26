7f1b242e42cd0e97e157b2199ecc9b52575f498d
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-26 16:08:51 +0545

    route for user after payment

---
+++ frontreact/src/components/afterlogin/PaymentCancel.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PaymentCancel = () => {
+    return(
+        <>
+          <h1>Your payment is successfull</h1>
+        </>
+    )
+}
+
+export default PaymentCancel;
---
+++ frontreact/src/components/afterlogin/PaymentSuccess.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PaymentSuccess = () => {
+    return(
+        <>
+          <h1 className="text-4xl text-center mt-[250px]">Your payment is successfull</h1>
+        </>
+    )
+}
+
+export default PaymentSuccess;
