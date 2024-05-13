c8f2c6eec61193f478155e1cdaaa2e8be0991aae
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-13 19:21:19 +0545

    post class for search configured

---
+++ backnode/src/controller/SearchController.js
@@ -0,0 +1,13 @@
+import Handler from "../logger/ResponseHandler.js"
+const responseInstance = new Handler();
+
+class SearchController {
+    async postSearch(req,res){
+        try{
+
+        }
+        catch(err){
+
+        }
+    }
+}
