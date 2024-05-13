f0f64ab20ff32b94712d9c0fb5a3325336f30313
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-13 19:29:20 +0545

    getsearch for fetching data

--- backnode/src/controller/SearchController.js
+++ backnode/src/controller/SearchController.js
@@ -2,12 +2,23 @@ import Handler from "../logger/ResponseHandler.js"
 const responseInstance = new Handler();
 
 class SearchController {
+
     async postSearch(req,res){
         try{
 
         }
         catch(err){
-
+           console.log(err,'error in inserting search data')
         }
     }
+
+    async getSearch(req,res){
+        try{
+
+        }
+        catch(err){
+           console.log(err,'error in fetching data')
+        }
+    }
+
 }
