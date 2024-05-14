a1b6c3de2dd0088584c4ca7404ca9541c99ad89d
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-14 18:37:11 +0545

    adding authentication

--- backnode/src/router/searchRouter.js
+++ backnode/src/router/searchRouter.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import SearchController from "../controller/SearchController.js"
+import authenticate from '../middleware/Authenticate.js';
 
 const searchRouter = express.Router();
 
 const searchInstance = new SearchController();
 
-searchRouter.post("/",searchInstance.postSearch);
+searchRouter.post("/",authenticate,searchInstance.postSearch);
 searchRouter.get("/",searchInstance.getSearch);
 searchRouter.delete("/",searchInstance.deleteSearch);
 
