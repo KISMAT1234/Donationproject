c4f078cbdcb8b1aaefa06b140303f2198a35edfb
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-15 14:17:51 +0545

    fetching data by category

--- backnode/src/controller/CategoryController.js
+++ backnode/src/controller/CategoryController.js
@@ -1,10 +1,21 @@
+import Post from "../model/Post.js";
+import Handler from "../logger/ResponseHandler.js";
+const responseInstance = new Handler();
+
 class CategoryController{
   async category(req,res){
     try{
-      
+      const categoryName= req.query.category
+      console.log(categoryName,'category name')
+      const categoryData = await Post.find({category: categoryName})
+      if(data){
+        return responseInstance.responseHandler(res,200,'data fetch success',categoryData)
+      }else{
+        return responseInstance.responseHandler(res,400,'there is no any  data')
+      }
     }
     catch{
-
+        return responseInstance.responseHandler(res,500,'server error')
     }
   }
 }
