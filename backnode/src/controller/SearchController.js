810f7b30a2b42192334b36f5b6ff949271704197
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-15 12:10:33 +0545

    sending response to backend

--- backnode/src/controller/SearchController.js
+++ backnode/src/controller/SearchController.js
@@ -13,16 +13,20 @@ class SearchController {
         const user = new Search({...req.body,userID: userId});
         console.log(user,'search value');
         await user.save();
+        return responseInstance.responseHandler(res, 200,"search data send  successfully");
           
         }
         catch(err){
            console.log(err,'error in inserting search data')
+           return responseInstance.responseHandler(res,200,'failed to fetch search data')
         }
     }
 
     async getSearch(req,res){
         try{
-            console.log("search post route")
+    
+
+            
         }
         catch(err){
            console.log(err,'error in fetching data')
