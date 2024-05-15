c701368c0f3ebc8b2ece0f48263d4ee1140b1555
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-15 12:16:00 +0545

    getting search data to show user

--- backnode/src/controller/SearchController.js
+++ backnode/src/controller/SearchController.js
@@ -6,27 +6,32 @@ class SearchController {
 
     async postSearch(req,res){
         try{
-          console.log("search post route")
-        //   const search = req.body;
-        let userId = req.user.userId
-        console.log(userId);
-        const user = new Search({...req.body,userID: userId});
-        console.log(user,'search value');
-        await user.save();
-        return responseInstance.responseHandler(res, 200,"search data send  successfully");
-          
+           console.log("search post route")
+           //   const search = req.body;
+           let userId = req.user.userId
+           console.log(userId);
+           const user = new Search({...req.body,userID: userId});
+           console.log(user,'search value');
+           await user.save();
+           return responseInstance.responseHandler(res, 200,"search data send  successfully");
         }
         catch(err){
            console.log(err,'error in inserting search data')
-           return responseInstance.responseHandler(res,200,'failed to fetch search data')
+           return responseInstance.responseHandler(res,500,'failed to fetch search data')
         }
     }
 
     async getSearch(req,res){
         try{
-    
-
-            
+            console.log("search post route")
+            let userId = req.user.userId
+            const searchData = await Search.find({userId: userId})
+            console.log(searchData,'fetched data');
+            if(searchData){
+                return responseInstance.responseHandler(res, 200,"search data fetched  successfully");
+            }else{
+              return responseInstance.responseHandler(res, 400,"there is no any history");
+            }   
         }
         catch(err){
            console.log(err,'error in fetching data')
