9be374285660da3f394f170e9cdc563614a37ed5
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-16 15:09:31 +0545

    leftbar ui change

--- frontreact/src/components/afterlogin/bar/Leftbar.jsx
+++ frontreact/src/components/afterlogin/bar/Leftbar.jsx
@@ -20,7 +20,7 @@ const logOut = () => {
 
 
     return <>
-     <div className="bg-gray-200  rounded-2xl mt-[20px]  md:px-5 h-[8vh] md:h-[80vh] justify-between flex md:block mx-5 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
+     <div className="bg-gray-200  rounded-2xl md:mt-[22px]  md:px-5 h-[8.5vh] md:h-[80vh] justify-between flex md:block mx-5 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
         <div className="py-2 md:py-3 md:text-2xl md:mt-5px font-bold text-md hover:text-slate-100 hover:bg-green-400 hover:rounded-2xl ">
            <button className="">
               <Link to ="/Mainpage" className="flex">
@@ -30,7 +30,7 @@ const logOut = () => {
            </button>
         </div>
 
-         <div className="py-2 md:py-3 md:text-2xl md:text-2xl md:mt-5px font-bold text-md hover:text-slate-100 hover:bg-green-400 hover:rounded-2xl ">
+         <div className="py-2 md:py-3 md:text-2xl  md:mt-5px font-bold text-md hover:text-slate-100 hover:bg-green-400 hover:rounded-2xl ">
              <button className="">
                <Link to ="/Mainpage/profile" className="flex">
                   <FaUser className=" mx-4 text-4xl md:mx-0 md:2xl"/>
@@ -39,7 +39,7 @@ const logOut = () => {
             </button>
         </div>
 
-         <div className="py-2 md:py-3 md:text-2xl md:text-2xl md:mt-5px font-bold text-md hover:text-slate-100 hover:bg-green-400 hover:rounded-2xl ">
+         <div className="py-2 md:py-3 md:text-2xl  md:mt-5px font-bold text-md hover:text-slate-100 hover:bg-green-400 hover:rounded-2xl ">
             <button className="">
               <Link to ="/Mainpage/favourites" className="flex">
                <FaStar className=" mx-4 text-4xl md:mx-0 md:2xl"/>
