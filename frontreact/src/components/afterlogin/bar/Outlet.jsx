53d7cec7caacb75e5f860265e5dbc5574f8a3134
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-17 14:40:48 +0545

    minor change

--- frontreact/src/components/afterlogin/bar/Outlet.jsx
+++ frontreact/src/components/afterlogin/bar/Outlet.jsx
@@ -9,7 +9,7 @@ const Layout = () => {
   return (
     <>
     <div className="">
-       <div className="fixed top-0 hidden md:block w-full">
+       <div className="fixed top-0  hidden md:block w-full">
          <Topbar />
        </div>
    
@@ -17,7 +17,7 @@ const Layout = () => {
            <div className=" fixed top-0 md:top-0  z-1000  md:w-[20%]  w-full md:mt-14">
               <Leftbar/>
            </div>
-           <div className="fixed mx-5 top-0 w-[90%] md:mt-20 md:w-[76%] md:ml-[20%] mt-14 ">
+           <div className="fixed mx-5 z-0 top-0 w-[90%] md:mt-20 md:w-[76%] md:ml-[20%] mt-14 ">
              <Category/>
            </div>
            <div className=" md:ml-[20%] mt-28 md:w-[100%] md:mt-32">
