a92eaf6041e94837549f19b22e295cdc5da315ea
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-18 18:47:58 +0545

    change in route

--- backnode/src/router/categoryRouter.js
+++ backnode/src/router/categoryRouter.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import CategoryController from "../controller/CategoryController.js"
-import authenticate from '../middleware/Authenticate.js';
+// import authenticate from '../middleware/Authenticate.js';
 
 
 const categoryRouter = express.Router();
 const categoryInstance = new CategoryController();
 
 
-categoryRouter.get("/:categoryName",authenticate,categoryInstance.category)
+categoryRouter.get("/",categoryInstance.category)
 
 
 export default categoryRouter;
