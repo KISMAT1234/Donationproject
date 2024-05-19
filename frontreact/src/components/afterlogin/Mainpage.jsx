291521e35e644f42ddf862e454d6201e8d337100
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-19 18:19:14 +0545

    minor change

--- frontreact/src/components/afterlogin/Mainpage.jsx
+++ frontreact/src/components/afterlogin/Mainpage.jsx
@@ -87,46 +87,91 @@ function Content() {
         <div className="flex text-4xl justify-center mt-[250px]">Loading content...</div>
       ) : (
         <div className="">
-          {content.map((data, index) => (
-            <div key={index} className="md:w-[90%] h-[70vh] px-5  ml-5 mr-5 py-5 mt-4 rounded-xl shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]">
-              <div className="flex justify-between">
-                <div className="flex">
-                   <div className="w-[10%] md:w-[20%]">
-                     <img src={data.userId.image} width="100" className=" rounded-[50%]" />
-                   </div>
-                   <div className="ml-5 font-extrabold  text-3xl">{data.userId.username}</div>
-                   <div className="text-xl ml-10 flex"> 
-                    <CiTimer />
-                    <h1 className="ml-1">{renderTime(data.createdAt)}</h1>
-                   </div>
-                  </div> 
-                 <div className="flex text-2xl">
-                   <TbDots className="mr-2"/>
-                   <CiSquareRemove />
-                 </div>
-            </div>
-              <div className="my-2 text-xl font-light">Name: {data.name}</div>
-              <div className="my-2 text-xl font-light">Address: {data.address}</div>
-              <div className="my-2 text-xl font-normal">End Date: {data.endDate}</div>
-              <div className="">
-                <img src={data.image} width="100" className="w-[90%]" alt="image" />
+          <div className="mx-5 flex fixed top-14 md:justify-between md:top-20 px-2 py-2 md:w-[72%] w-[94%] rounded-2xl bg-stone-100 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
+              <Link to="/Mainpage">
+              <div className="ml-1 my-1 px-1 py-1 rounded-2xl bg-gray-400 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
+                <h1 className="text-sm md:text-xl">All</h1>
               </div>
-              <div className="mt-5 flex justify-between">
-                <div>
-                  <Link to={"donate/" + data._id}>
-                    <button className="bg-green-500 h-[45px] text-2xl w-[120%] rounded hover:bg-red-600">
-                      Donate Now
+              </Link>
+      
+              <Link to = "/Mainpage/search?category=Accident">
+              <div className="ml-1 my-1 px-1 py-1 rounded-2xl bg-gray-400 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
+                <h1 className="text-sm md:text-xl">Accident</h1>
+              </div>
+              </Link>
+      
+                <Link to ="">
+              <div className="ml-1 my-1 px-1 py-1 rounded-2xl bg-gray-400 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
+                <h1 className="text-sm md:text-xl">Disease</h1>
+              </div>
+              </Link>
+        
+              <Link to ="">
+              <div className="ml-1 my-1 px-1 py-1 rounded-2xl bg-gray-400 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
+                <h1 className="text-sm md:text-xl">Animal</h1>
+              </div>
+              </Link>
+        
+              <Link to ="">
+              <div className="ml-1 my-1 px-1 py-1 rounded-2xl bg-gray-400 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
+                <h1 className="text-sm md:text-xl">Environment</h1>
+              </div>
+              </Link>
+        
+              <Link to ="/Mainpage/search?category=Orphange">
+              <div className="ml-1 my-1 px-1 py-1 rounded-2xl bg-gray-400 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
+                <h1 className="text-sm md:text-xl">Orphange</h1>
+              </div>
+              </Link>
+
+              <Link to ="">
+              <div className="ml-1 my-1 px-1 py-1 rounded-2xl bg-gray-400 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
+                <h1 className="text-sm md:text-xl">Other</h1>
+              </div>
+              </Link>
+            </div>
+          <div className="mt-28 md:mt-20">
+            {content.map((data, index) => (
+              <div key={index} className="md:w-[90%] h-[70vh] px-5  ml-5 mr-5 py-5 mt-2 rounded-xl shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]">
+                <div className="flex justify-between">
+                  <div className="flex">
+                     <div className="w-[10%] md:w-[20%]">
+                       <img src={data.userId.image} width="100" className=" rounded-[50%]" />
+                     </div>
+                     <div className="ml-5 font-extrabold  text-3xl">{data.userId.username}</div>
+                     <div className="text-xl ml-10 flex"> 
+                      <CiTimer />
+                      <h1 className="ml-1">{renderTime(data.createdAt)}</h1>
+                     </div>
+                    </div> 
+                   <div className="flex text-2xl">
+                     <TbDots className="mr-2"/>
+                     <CiSquareRemove />
+                   </div>
+              </div>
+                <div className="my-2 text-xl font-light">Name: {data.name}</div>
+                <div className="my-2 text-xl font-light">Address: {data.address}</div>
+                <div className="my-2 text-xl font-normal">End Date: {data.endDate}</div>
+                <div className="">
+                  <img src={data.image} width="100" className="w-[90%]" alt="image" />
+                </div>
+                <div className="mt-5 flex justify-between">
+                  <div>
+                    <Link to={"donate/" + data._id}>
+                      <button className="bg-green-500 h-[45px] text-2xl w-[120%] rounded hover:bg-red-600">
+                        Donate Now
+                      </button>
+                    </Link>
+                  </div>
+                  <div>
+                    <button onClick={() => onSubmit(data, index)}>
+                      <CiStar className="text-4xl" style={{ fill: clickedIndex === index ? 'black' : 'blue', backgroundColor: clickedIndex === index ? 'yellow' : 'transparent' }} />
                     </button>
-                  </Link>
-                </div>
-                <div>
-                  <button onClick={() => onSubmit(data, index)}>
-                    <CiStar className="text-4xl" style={{ fill: clickedIndex === index ? 'black' : 'blue', backgroundColor: clickedIndex === index ? 'yellow' : 'transparent' }} />
-                  </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
             <div className="my-20 flex justify-center">
         <Pagination current={current} onChange={onChange} total={50} />
           </div>
