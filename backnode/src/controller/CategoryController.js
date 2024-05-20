bb801ba68aab32b117fd64b18e11a7b7c4814eba
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-20 14:08:43 +0545

    consoling value

--- backnode/src/controller/CategoryController.js
+++ backnode/src/controller/CategoryController.js
@@ -6,9 +6,9 @@ class CategoryController{
   async category(req,res){
     try{
       const categoryName= req.query.categoryName;
-      console.log(categoryName,'category name')
+      // console.log(categoryName,'category name')
       const categoryData = await Post.find({category: categoryName})
-      console.log(categoryData,'cat data');
+      // console.log(categoryData,'cat data');
       if(categoryData){
         return responseInstance.responseHandler(res,200,'data fetch success',categoryData)
       }else{
