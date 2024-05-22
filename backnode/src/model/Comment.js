fb918bb9e71d6e069810376dc70b1e12879abe80
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-22 17:23:44 +0545

    adding user infromation in an array

--- backnode/src/model/Comment.js
+++ backnode/src/model/Comment.js
@@ -18,7 +18,8 @@ const commentSchema = new mongoose.Schema({
         type:Number,
         default:0
     },
-    
+    likedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    dislikeBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
     createdAt: {
         type: String,
         default: () => {
