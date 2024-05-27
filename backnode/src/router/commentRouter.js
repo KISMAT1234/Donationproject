fd0e01f1aa99c433b1e1727ec1f8fe3d23898c0f
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-27 14:20:38 +0545

    delete route for sending id of specific comment

--- backnode/src/router/commentRouter.js
+++ backnode/src/router/commentRouter.js
@@ -13,5 +13,7 @@ cmtRouter.post("/dislike/:id",authenticate,commentInstance.postDisLike)
 cmtRouter.get("/getLike",commentInstance.getLike)
 cmtRouter.get("/getDisLike/:id",commentInstance.postDisLike)
 // cmtRouter.get("/dislike/:id",authenticate,commentInstance.getLike)
+cmtRouter.delete("/:id", commentInstance.deleteComment)
+
 
 export default cmtRouter;
