ee045343e08e9d90b540e01f2e033914f9e4f3f9
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-01 16:46:01 +0545

    update and private button aadded

--- frontreact/src/components/afterlogin/Profile.jsx
+++ frontreact/src/components/afterlogin/Profile.jsx
@@ -63,9 +63,10 @@ const Profile = () => {
     <div className="mx-5 my-10 rounded-2xl shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
       <div className="flex justify-between">
        <Link to = {"/Mainpage/profile/" + profile[0]?.slug}>
-        <button className="bg-red-600  hover:bg-green-600 rounded-xl px-2 py-1 mx-5 my-5 justify-end">
+        <button className="bg-green-500 flex hover:bg-green-700 hover:text-stone-800 rounded-xl px-2 py-1 mx-5 my-5 justify-end">
             {/* <h1>Edit</h1> */}
             <FaEdit className="ml-1 text-4xl" />
+            <h1 className="text-4xl">Edit Profile</h1>
         </button>
        </Link>
 
@@ -93,22 +94,37 @@ const Profile = () => {
        <h1 className="text-4xl font-mono">Post</h1>
        {
         post.map((postData, index)=>{
-           return <div key={index} className="pt-1 px-5 rounded-2xl shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+          return <div key={index} className="pt-1 px-5 rounded-2xl shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
             <div className="flex justify-between">
-                <h1 className=" my-5">Name: {postData.name}</h1>
-                <h1 className=" my-5">Address: {postData.address}</h1>
-                <h1 className=" my-5">Age {postData.age}</h1>
-                <button onClick={()=> deletePost(postData._id)} className=" px-5 h-10 rounded-xl bg-red-500  hover:bg-red-600 text-xl hover:text-slate-200 ">Delete</button>
+              <div className="w-[70%]">
+                <div className="flex flex-wrap justify-between">
+                    <h1 className=" my-5">Name: {postData.name}</h1>
+                    <h1 className=" my-5">Address: {postData.address}</h1>
+                    <h1 className=" my-5">Age {postData.age}</h1>
+                </div>
+                <div className="">
+                    <h1 className=" my-5">Phone: {postData.phone}</h1>
+                    <h1 className=" my-5">StartDate: {postData.startDate}</h1>
+                    <h1 className=" my-5">EndDate: {postData.endDate}</h1>
+                    <h1 className=" my-5">Category: {postData.category}</h1>
+                </div>
+              </div>
+              <div className="ml-50">
+                <div>
+                  <button onClick={()=> deletePost(postData._id)} className=" px-5 h-10 rounded-xl bg-green-500  hover:bg-green-600 text-xl hover:text-stone-100 ">Update</button>
+                </div>
+                <div>
+                  <button onClick={()=> deletePost(postData._id)} className=" px-5 h-10 my-2 rounded-xl bg-red-500  hover:bg-red-600 text-xl hover:text-slate-100 ">Delete</button>
+                </div>
+                <div>
+                  <button onClick={()=> deletePost(postData._id)} className=" px-5 h-10 my-2 rounded-xl bg-yellow-500  hover:bg-yellow-600 text-xl hover:text-slate-100 ">Private</button>
+                </div>
+              </div>
             </div>
-            <div className="">
-                <h1 className=" my-5">Phone: {postData.phone}</h1>
-                <h1 className=" my-5">StartDate: {postData.startDate}</h1>
-                <h1 className=" my-5">EndDate: {postData.endDate}</h1>
-                <h1 className=" my-5">Category: {postData.category}</h1>
                 <h1 className=" my-5">Description: <br/>{postData.description}</h1>
                 <h1 className=" my-5">Image </h1>
-            </div>
-           </div>
+            
+          </div>
         })
        }
     </div>
