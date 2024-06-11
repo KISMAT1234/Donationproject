f954eb55243445ad402244d837906b72b623a88e
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-11 13:58:53 +0545

    componenet for notification

---
+++ frontreact/src/components/afterlogin/Notification.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Notification = () => {
+  return (
+    <>
+    <div className="mt-5">
+        <div>
+            <h1>Notification</h1>
+        </div>
+    </div>
+  
+    </>
+  )
+}
+
+export default Notification
