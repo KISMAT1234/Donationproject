18966735765cfb4c227760eebb67f66341da6352
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-11 13:59:21 +0545

    socket io client configured

--- frontreact/src/main.jsx
+++ frontreact/src/main.jsx
@@ -5,6 +5,20 @@ import App from './App.jsx'
 import "./index.css"
 import { store } from './store/store'
 import { Provider } from 'react-redux'
+import { io } from 'socket.io-client'
+
+
+export const socket = io(import.meta.env.BackendServer)
+console.log(socket,'socket url')
+
+socket.on('connect', () => {
+  console.log(socket.id) // x8WIv7-mJelg7on_ALbx
+})
+
+socket.on('disconnect', () => {
+  console.log(socket.id) // undefined
+})
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
