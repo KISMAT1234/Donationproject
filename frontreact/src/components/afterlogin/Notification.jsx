dcd859169fd84a426f7358c2dfa2cdb88d2e8bac
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-12 18:35:40 +0545

    getting value and connection in notification component

--- frontreact/src/components/afterlogin/Notification.jsx
+++ frontreact/src/components/afterlogin/Notification.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { socket } from '../../main';
 
 const Notification = () => {
-  return (
-    <>
-    <div className="mt-5">
-        <div>
-            <h1>Notification</h1>
-        </div>
-    </div>
-  
-    </>
-  )
-}
+  const [notifications, setNotifications] = useState([]);
 
-export default Notification
+  useEffect(() => {
+  
+    console.log('came in notification component')
+    socket.on('notification', (notification) => {
+      console.log(notification,'socket connection successfull in frontend')
+      setNotifications((prevNotifications) => [notification, ...prevNotifications]);
+    });
+
+    return () => {
+      socket.off('notification')
+    };
+  }, []);
+
+  return (
+    <div>
+      <h3>Notifications</h3>
+      <ul>
+        {notifications.map((notification) => (
+          <li key={notification._id}>{notification.message}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Notification;
