01167db3a91a39a6365ff189ea39a8c13b53fa9d
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-12 18:33:44 +0545

    getting userid token

--- frontreact/src/main.jsx
+++ frontreact/src/main.jsx
@@ -6,13 +6,24 @@ import "./index.css"
 import { store } from './store/store'
 import { Provider } from 'react-redux'
 import { io } from 'socket.io-client'
+import {jwtDecode} from 'jwt-decode';
+
+const token = localStorage.getItem('token');
+const decodedToken = jwtDecode(token);
+const userId = decodedToken.id
 
 
-export const socket = io(import.meta.env.BackendServer)
-console.log(socket,'socket url')
+// export const socket = io(import.meta.env.BackendServer).emit('join', 'userId')
+export const socket = io('http://localhost:8000', {
+    userId: { userId },
+    withCredentials: true,
+});
+// console.log(socket,'socket url')
+
 
 socket.on('connect', () => {
   console.log(socket.id) // x8WIv7-mJelg7on_ALbx
+  socket.emit('join', userId);
 })
 
 socket.on('disconnect', () => {
