c43792348e852e9b9c82beb224888409d87360ba
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-12 18:32:55 +0545

    minor change

--- backnode/src/router/notificationRouter.js
+++ backnode/src/router/notificationRouter.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import NotificationController from "../controller/NotificationController.js"
+// import NotificationController from "../controller/NotificationController.js"
 import authenticate from '../middleware/Authenticate.js';
 
 const notificationRouter = express.Router();
 
-const notificationInstance = new NotificationController();
+// const notificationInstance = new NotificationController();
 
-notificationRouter.get("/",authenticate,notificationInstance.getNotification);
+// notificationRouter.get("/",authenticate,notificationInstance.getNotification);
 
 
 export default notificationRouter;
