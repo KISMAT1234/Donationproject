eb9c343e1b5d47d505e01b8bbf68122c3e21ef33
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-13 17:55:56 +0545

    adding notification to send and emmited successfuly to user

--- backnode/socket.js
+++ backnode/socket.js
@@ -1,19 +1,57 @@
 import { Server } from 'socket.io';
 import { createServer } from 'http';
+import dotenv from 'dotenv';
+import User from './src/model/Userprofile.js';
+import Post from './src/model/Post.js';
+import Notification from "./src/model/Notification.js";
+
+
+
+dotenv.config();
+
 
 const initializeSocket = (app) => {
+   
   const httpServer = createServer(app);
 
   const io = new Server(httpServer, {
     cors: {
-      origin: process.env.FRONTEND_URL, // Replace with your frontend URL
+      origin:"http://localhost:5173", // Replace with your frontend URL
       methods: ['GET', 'POST'],
       credentials: true,
     },
   });
 
   io.on('connection', (socket) => {
+    
     console.log('A user connected');
+    // io.emit("notification",'testing connection')
+    socket.on('notification', async ({postId,userId})=>{
+      const post = await Post.findById(postId)
+      console.log(post,'user post')
+
+      const user = await User.findById(userId)
+
+      const postUserId = post.userId._id.toString()
+      console.log(postUserId,'user id')
+
+      // if (post && postUserId !== userId) {
+         const notification = new Notification({
+             sender: userId,
+             receiver: postUserId,
+             type: 'comment',
+             message: `${user.username} commented on your post`,
+             postId:postId
+         });
+         console.log(notification,'notification to send user and save')
+         await notification.save();
+   
+         io.emit('notification', notification);
+        // console.log(postId,'post id')
+        // io.emit('notification',postId)
+      // }
+    })
+  
 
     socket.on('disconnect', () => {
       console.log('User disconnected');
