a3aad9ddcddf8ff271d273b25bee92df157949aa
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-14 14:15:53 +0545

    sidebar container integrated

--- frontreact/src/components/afterlogin/bar/Leftbar.jsx
+++ frontreact/src/components/afterlogin/bar/Leftbar.jsx
@@ -6,21 +6,38 @@ import { FaUsers } from "react-icons/fa";
 import { FaSignOutAlt } from "react-icons/fa";
 import { FaUser } from "react-icons/fa";
 import { RxTextAlignJustify } from "react-icons/rx";
+import { useState } from "react";
 
 
 
 
 function  Leftbar(){
+   const[open, setOpen] = useState(false);
 
 const sidebar = () => {
- 
+   setOpen(!open)
 }
+
+
+
 
 
 
 
     return <>
          <div>
+         {open && (
+      <div className="h-[100vh] px-5 py-5 w-[60%] bg-red-600 z-50   md:block absolute transform transition-transform duration-300 ease-in-out ">
+        <div className="flex justify-between">
+          <a href="#" className="text-xl font-bold md:absolute md:block ">Mystry message</a>
+          <button onClick={sidebar}>
+            {/* <X className="text-4xl"/> */}
+            <h1>X</h1>
+          </button>
+        </div>
+     
+      </div>
+    )}
             <div className="bg-violet-600  rounded-2xl md:mt-[22px]  md:px-5 h-[8.5vh] md:h-[56vh] justify-between flex md:block mx-5 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
                <div className="py-2 md:py-3 md:text-2xl md:mt-5px font-bold text-md hover:text-slate-100 hover:bg-green-400 hover:rounded-2xl ">
                      <Link to ="/Mainpage" className="flex">
