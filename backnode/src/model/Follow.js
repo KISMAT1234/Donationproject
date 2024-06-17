e0379d76ebe720e5d6c0f591260f0cefe71f82bb
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-17 15:21:15 +0545

    adding date

--- backnode/src/model/Follow.js
+++ backnode/src/model/Follow.js
@@ -8,7 +8,23 @@ const followSchema = new mongoose.Schema({
     following:{
         type: mongoose.Schema.Types.ObjectId,
         ref:"User"
-    }    
+    },
+    createdAt: {
+        type: String,
+        default: () => {
+          const date = new Date();
+          const monthNames = [
+            'January', 'February', 'March',
+            'April', 'May', 'June', 'July',
+            'August', 'September', 'October',
+            'November', 'December'
+          ];
+          const day = date.getDate();
+          const monthIndex = date.getMonth();
+          const year = date.getFullYear();
+          return `${monthNames[monthIndex]} ${day}, ${year}`;
+        }
+    }
 })
 
 export default mongoose.model("Follow", followSchema);
