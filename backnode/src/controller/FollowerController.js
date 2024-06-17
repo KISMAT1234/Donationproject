e95a13745a73ebb7e6cec799da8afc152a227b07
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-17 15:20:53 +0545

    getting userdata

--- backnode/src/controller/FollowerController.js
+++ backnode/src/controller/FollowerController.js
@@ -1,76 +1,15 @@
 import User from "../model/Userprofile.js"
+import Handler from "../logger/ResponseHandler.js"
+const responseInstance = new Handler();
 
 class FollowController{
    async  getUserProfile(req,res){
     try{
-        const followerId = req.user.userId;
-        const followingId = req.params.id;
-        const user = new User({follower:followerId, following:followingId});
-        let following = user.following;
-        
-        following.push(user._id);
+        const followId = req.params.id;
+        const userId = req.user.id;
+    }
+    catch(error){
 
-     const people = await User.find({_id: {$nin:following}})
-
-    //  const {username} = req.params
-
-    //  const channel = await User.aggregate([
-    //     {
-    //         $match:{
-    //             username: username?.toLowerCase()
-    //         }
-    //     },
-    //     {
-    //         $lookup:{
-    //             from: "Follow",
-    //             localfield:"_id",
-    //             foreignField:"follower",
-    //             as:"followers"
-    //         }
-    //     },
-    //     {
-    //         $lookup:{
-    //             from: "Follow",
-    //             localfield:"_id",
-    //             foreignField:"following",
-    //             as:"followedTo"
-    //         }
-    //     },
-    //     {
-    //         $addFields: {
-    //             followerCount:{
-    //                 $size:"followers"
-    //             },
-    //             followingCount:{
-    //                 $size:"followedTo"
-    //             },
-    //             isFollowed:{
-    //                 $cond:{
-    //                     if:{$in: [req.user?._id,"$follower.follower"]}
-    //                 }
-    //             }
-    //         }
-    //     },
-    //     {
-    //         $project:{
-    //             fullName: 1,
-    //             username: 1,
-    //             followerCount: 1,
-    //             followingCount: 1,
-    //             isFollowed: 1,
-    //             email: 1
-                
-    //         }
-    //     }
-
-    //  ])
-
-    //  if(!channel?.length){
-    //     console.log("Channel not found");
-    //  }
-
-    }catch(err){
-        console.log(err)
     }
 
    }
