2f32a285cb9697435c1657e36f39cdf5a7a91da2
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-18 14:51:06 +0545

    saving follow details in database

--- backnode/src/controller/FollowerController.js
+++ backnode/src/controller/FollowerController.js
@@ -1,12 +1,41 @@
 import User from "../model/Userprofile.js"
 import Handler from "../logger/ResponseHandler.js"
+import Follow from "../model/Follow.js";
 const responseInstance = new Handler();
+
 
 class FollowController{
    async  getUserProfile(req,res){
     try{
-        const followId = req.params.id;
-        const userId = req.user.id;
+        const followerId = req.params.id;
+        console.log(followerId,'follower id')
+        const followingId = req.user.userId;
+        console.log(followingId,'following id')
+
+
+        const user = await User.findById(followerId);
+        const followUser = await User.findById(followingId);
+
+
+        if (!user || !followUser) {
+          return responseInstance.responseHandler(res,400,'User not found')
+        }
+
+        const existingFollow = await Follow.findOne({ follower: followerId, following: followingId });
+
+        if(!existingFollow){
+
+            const newFollow = new Follow({ follower: followerId, following: followingId });
+            console.log(newFollow,'new follow')
+            // await newFollow.save();
+            return responseInstance.responseHandler(res,200,'Following to user SUccessfulll')
+        }else{
+            await Follow.findOneAndDelete({ follower: followerId, following: followingId });
+            return responseInstance.responseHandler(res,200,'unfolled to user',)
+        
+        }
+
+  
     }
     catch(error){
 
