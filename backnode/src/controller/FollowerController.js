7d370fa6355fb2e2340ceefe66f4435e7544835a
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-19 15:19:48 +0545

    saving data of followers success

--- backnode/src/controller/FollowerController.js
+++ backnode/src/controller/FollowerController.js
@@ -8,9 +8,9 @@ class FollowController{
    async  getUserProfile(req,res){
     try{
         const followerId = req.params.id;
-        console.log(followerId,'follower id')
+        // console.log(followerId,'follower id')
         const followingId = req.user.userId;
-        console.log(followingId,'following id')
+        // console.log(followingId,'following id')
 
 
         const user = await User.findById(followerId);
@@ -24,14 +24,13 @@ class FollowController{
         const existingFollow = await Follow.findOne({ follower: followerId, following: followingId });
 
         if(!existingFollow){
-
             const newFollow = new Follow({ follower: followerId, following: followingId });
             console.log(newFollow,'new follow')
-            // await newFollow.save();
+            await newFollow.save();
             return responseInstance.responseHandler(res,200,'Following to user SUccessfulll')
         }else{
             await Follow.findOneAndDelete({ follower: followerId, following: followingId });
-            return responseInstance.responseHandler(res,200,'unfolled to user',)
+            return responseInstance.responseHandler(res,200,'unfolled to user')
         
         }
 
