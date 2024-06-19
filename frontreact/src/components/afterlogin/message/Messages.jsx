ce926952999043ebcd59c2fbd9a28ab0f0d1e2a0
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-19 20:20:34 +0545

    importing conversation and sidebar

--- frontreact/src/components/afterlogin/message/Messages.jsx
+++ frontreact/src/components/afterlogin/message/Messages.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Conversation from './Conversation';
+import Sidebar from './Sidebar';
 
 const Messages = () => {
   return (
     <>
       <div>Messages</div>
     </>
-  
   )
 }
 
