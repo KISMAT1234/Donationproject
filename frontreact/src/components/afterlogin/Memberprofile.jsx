7b8fe6dfcba64f6e033e39abc63fb8819754b52b
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-19 15:21:10 +0545

    sending data here while following

--- frontreact/src/components/afterlogin/Memberprofile.jsx
+++ frontreact/src/components/afterlogin/Memberprofile.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import React,{useState, useEffect} from "react";
 import axiosUrl from "../url/Axiosurl";
 import { FaPhoneAlt } from "react-icons/fa";
@@ -12,21 +12,33 @@ const Userprofile = () => {
    const {slug} = useParams()
    const [user, setUser] = useState([]);
    const [post, setPost] = useState([]);
+   const [follow, setFollow] = useState(false);
   //  const [follow, setFollow] = useState(false);
    useEffect(() => {
       const getSingleUser = async () => {
          try {
             const response = await axiosUrl.get(`/user/${slug}`);
-            console.log(response.data.data,'response data');
-            setUser(response.data.data.user);
+            console.log(response.data.data,'response user');
+            setUser([response.data.data.user]);
             setPost(response.data.data.post);
-            
+            setFollow(response.data.data.isFollowed);
          } catch (err) {
             console.error("Error fetching user data:", err);
          }
       };
       getSingleUser();
    },[slug]);
+
+   const onFollow = (id) => {
+     setFollow(!follow)
+    axiosUrl.post(`/follow/${id}`).then((response)=>{
+     console.log(response.data.data,'response of follow ');
+    //  setFollow(response.data.data.isFollowing);
+    }).catch((err) => {
+     console.log(err,'error');
+    })
+
+}
 
     return (
         <>
@@ -42,23 +54,13 @@ const Userprofile = () => {
               user.map((data, index)=>(
                 <div key={index}>
                    <div className="md:flex justify-between">
-                     <div className="text-center bg-stone-200 md:w-[30%] text-4xl font-mono font rounded-2xl px-5 py-5 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
+                     <div className="text-center bg-stone-200 md:w-[30%] text-4xl md:text-2xl font-mono font rounded-2xl px-5 py-5 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
                         <h1 className="mt-5 ">Image</h1>
-                        <h1 className="mt-5 ">{data.username}</h1>
-                        <h1 className="mt-5 ">Bio</h1>
-                        {/* {
-                          follow ? (
-                            <h1>Following</h1>
-                          ) : (
-                            <h1 className="mt-5 ">Follow</h1>
-                          )
-                        } */}
+                        <h1 className="mt-5 ">Name:{data.username}</h1>
+                        <h1 className="mt-5 ">ProfileViews:{data.profileViews}</h1>
                         <h1 className="mt-5 ">Message</h1>
                      </div>
-
-                
-
-                     <div className=" bg-pink-600 md:w-[65%] text-4xl font-mono font rounded-2xl px-5 py-5 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
+                     <div className=" bg-gray-400 md:w-[65%] text-4xl font-mono font rounded-2xl px-5 py-5 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
                        <div className="flex mt-5">
                         <FaPhoneAlt />:
                         <h1>+977 9812345678</h1>
@@ -74,6 +76,25 @@ const Userprofile = () => {
                        <div className="flex mt-5">
                          <MdOutlineEmail />:
                          <h1>{data.email}</h1>
+                       </div>
+                       <div className="mt-5 flex justify-between">
+                          { follow ? (
+                              <button onClick={()=>onFollow(data._id)}> 
+                                <h1 className="text-white  bg-red-500 rounded-2xl px-2 py-2">unFollow</h1>
+                              </button>
+                          ) : (
+
+                            <button onClick={()=>onFollow(data._id)} className="bg-green-600 hover:bg-green-500 hover:scale-110 px-2 py-2 rounded-2xl text-white">
+                               <h1>Follow</h1>
+                            </button>
+                          )
+                          }
+                          <div>
+                            <Link to="/messages">
+
+                              <h1 className="bg-green-600 hover:bg-green-500 hover:scale-110 px-2 py-2 rounded-2xl text-white">Send Message</h1>
+                            </Link>
+                          </div>
                        </div>
                      </div>
                    </div>
