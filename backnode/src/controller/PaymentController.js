4b2ef9f5031ef5b781b3b754df93eedf094540e2
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-20 19:42:26 +0545

    adding amount data and user information

--- backnode/src/controller/PaymentController.js
+++ backnode/src/controller/PaymentController.js
@@ -17,22 +17,34 @@ class PaymentController{
     async pay(req,res){
         try{
 
-            const info = req.body
-            // console.log(info,'info result');
+            const paymentData= req.body
+            console.log(paymentData.postData.name,'payment data');
+            // console.log(paymentData[0].name,'payment data');
+            console.log(paymentData,'payment data');
+
+            const userId = req.user.userId
+            console.log(userId)
 
             const session = await stripeInstance.checkout.sessions.create({
               payment_method_types:["card"],
+              submit_type:'pay',
               mode:'payment',
-              line_items:[{
+              customer_email:paymentData.postData.userId.email,
+              line_items: [{
                 price_data: {
                   currency: "usd",
                   product_data: {
-                    name:"hari"
+                    name: paymentData.postData.name,
+                    // images:'Image', 
+                    // metadata:{
+                    //   postId: paymentData.postId
+                    // },
                   },
-                  unit_amount: 2000,
+                  unit_amount: 1000, 
                 },
                 quantity: 1,
               }],
+              // unit_amount: 0,
               mode:"payment",
               success_url: `${process.env.FRONTEND_URL}/payment-success`,
               cancel_url: `${process.env.FRONTEND_URL}/payment-cancel`,
