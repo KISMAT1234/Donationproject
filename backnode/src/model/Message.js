ce8f81b070fa131f19a138e7c76c0780784f3afc
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-20 13:28:53 +0545

    message attribute added with messages

--- backnode/src/model/Message.js
+++ backnode/src/model/Message.js
@@ -1,5 +1,18 @@
 import mongoose from 'mongoose';
 const messageSchema = new mongoose.Schema({
-
+    senderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    receiverId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    message: {
+        type: String,
+        required: true,
+    },
 })
 export default mongoose.model("Message", messageSchema);
