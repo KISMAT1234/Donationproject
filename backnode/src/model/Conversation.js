37dc735b3a7420a192224d108ab27461a54299de
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-20 17:04:01 +0545

    adding converstion attribute

---
+++ backnode/src/model/Conversation.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+const conversationSchema = new mongoose.Schema({
+    participants: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+    ],
+    messages: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Message",
+            default: [],
+        },
+    ],
+})
+export default mongoose.model("Conversation", conversationSchema);
