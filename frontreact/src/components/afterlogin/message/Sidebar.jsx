47b06ad83ca8f3d95ee68ac38ee9bf2ba164a081
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-20 13:58:58 +0545

    adding axiosUrl

--- frontreact/src/components/afterlogin/message/Sidebar.jsx
+++ frontreact/src/components/afterlogin/message/Sidebar.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axiosUrl from "../../url/Axiosurl";
+
+
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
+  const[user, setUser] = useState([])
+
+  useEffect(() => {
+     axiosUrl.get('user').then((response)=>{
+      console.log(response.data.data);
+     }) 
+  },[])
+    return (
+    <>
+      <div>Sidebar</div>
+      <div>
+
+      </div>
+    </>
   )
 }
 
