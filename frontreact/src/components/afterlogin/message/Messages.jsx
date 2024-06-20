44f6208fa00f46d52d7effbd72dcb4021a03bd93
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-20 17:04:34 +0545

    adding tailwind css color

--- frontreact/src/components/afterlogin/message/Messages.jsx
+++ frontreact/src/components/afterlogin/message/Messages.jsx
@@ -5,7 +5,16 @@ import Sidebar from './Sidebar';
 const Messages = () => {
   return (
     <>
-      <div>Messages</div>
+        {/* <div>Messages</div> */}
+      <div className="flex px-10 py-10">
+        <div className="w-[25%] h-[80vh] bg-red-500">
+          <Sidebar/>
+        </div>
+        <div className="w-[75%] h-[80vh] bg-green-500">
+           <Conversation />
+        </div>
+      </div>
+      
     </>
   )
 }
