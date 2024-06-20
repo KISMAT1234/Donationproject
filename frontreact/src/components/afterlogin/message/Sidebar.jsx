3455ad8a01a37c8ef4a8252eacdad4e26dadc14e
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-20 17:04:57 +0545

    rendering user data fetched from backend

--- frontreact/src/components/afterlogin/message/Sidebar.jsx
+++ frontreact/src/components/afterlogin/message/Sidebar.jsx
@@ -9,13 +9,32 @@ const Sidebar = () => {
   useEffect(() => {
      axiosUrl.get('user').then((response)=>{
       console.log(response.data.data);
+      setUser(response.data.data);
      }) 
   },[])
+
+  const userClick = () => {
+    console.log('clicked');
+  }
     return (
     <>
       <div>Sidebar</div>
       <div>
-
+         {
+          user.map((userList)=>{
+            return(
+              <>
+                <button onClick={userClick()} className="mx-2 my-4 px-2 py-2  flex justify-between hover:bg-gray-300 ">
+                  <div className="flex md:text-4xl">
+                    <h1>Image</h1>
+                    <h1 className="mx-2 text-xl font-light">{userList.username}</h1>
+                  </div>
+                  <h1>...</h1>
+                </button>
+              </>
+            )
+          })
+         }
       </div>
     </>
   )
