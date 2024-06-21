d5f9d1f634abe2ac4ac7fbd997470385eb708d1f
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-21 16:49:47 +0545

    rendering message

--- frontreact/src/components/afterlogin/message/Conversation.jsx
+++ frontreact/src/components/afterlogin/message/Conversation.jsx
@@ -1,31 +1,45 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axiosUrl from '../../url/Axiosurl'
 
 const Conversation = () => {
   const[conversation, setConversation] = useState([])
-  const[currentChat, setCurrentChat] = useState(null)
+  const[currentChat, setCurrentChat] = useState(null) 
+  const[messages,setMessages ] = useState([])
 
   useEffect(()=>{
     axiosUrl.get('/message').then((response)=>{
-        console.log(response.data.data)
+        console.log(response.data.data,'response message data')
+        setMessages(response.data.data)
     }).catch((error)=>{
       console.log(error)
     })
-  })
+  },[])
 
   return (
     <>
       { 
-       currentChat ? (
+       !currentChat ? (
         
-         <div className="flex flex-col">
-           <div className="">
+         <div className="">
+           <div className="h-[10vh] bg-yellow-500">
              top user data
            </div>
-           <div className="">
+           <div className="h-[60vh]">
              center message
+             {
+              messages.map((data,index)=>{
+                return(
+                  <>
+                    <div key={index}>
+                      <h1>{data.message}</h1>
+                      
+                    </div>
+                  </>
+                )
+              })
+             }
            </div>
-           <div className="h-[10vh]">
+           <div className="h-[10vh] bg-gray-500">
              footer input field
            </div>
          </div>
