0ba803e5f0696aa0aa6b9f3715897833e1194d4c
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-21 18:58:50 +0545

    time format in readable added

--- backnode/src/model/Message.js
+++ backnode/src/model/Message.js
@@ -14,5 +14,22 @@ const messageSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    createdAt: {
+        type: String,
+        default: () => {
+          const date = new Date();
+          const monthNames = [
+            'January', 'February', 'March',
+            'April', 'May', 'June', 'July',
+            'August', 'September', 'October',
+            'November', 'December'
+          ];
+          const day = date.getDate();
+          const monthIndex = date.getMonth();
+          const year = date.getFullYear();
+          return `${monthNames[monthIndex]} ${day}, ${year}`;
+        }
+    }
+
 })
 export default mongoose.model("Message", messageSchema);
