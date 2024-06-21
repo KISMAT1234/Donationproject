e82ccb343adbc8830d315f8972f1e639bffd81a1
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-21 18:57:46 +0545

    getting token and customize ui

--- frontreact/src/components/afterlogin/message/Conversation.jsx
+++ frontreact/src/components/afterlogin/message/Conversation.jsx
@@ -1,11 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import { Input } from 'antd';
 import axiosUrl from '../../url/Axiosurl'
+import {jwtDecode} from 'jwt-decode';
+
 
 const Conversation = () => {
   const[conversation, setConversation] = useState([])
   const[currentChat, setCurrentChat] = useState(null) 
   const[messages,setMessages ] = useState([])
+
+  const token = localStorage.getItem('token');
+  let currentUserId;
+
+  if (token) {
+    const decodedToken = jwtDecode(token);
+    currentUserId = decodedToken.id;
+  }
+  // console.log(userId,'userId');
 
   useEffect(()=>{
     axiosUrl.get('/message').then((response)=>{
@@ -18,42 +29,34 @@ const Conversation = () => {
 
   return (
     <>
-      { 
-       !currentChat ? (
-        
-         <div className="">
-           <div className="h-[10vh] bg-yellow-500">
-             top user data
-           </div>
-           <div className="h-[60vh]">
-             center message
-             {
-              messages.map((data,index)=>{
-                return(
-                  <>
-                    <div key={index}>
-                      <h1>{data.message}</h1>
-                      
-                    </div>
-                  </>
-                )
-              })
-             }
-           </div>
-           <div className="h-[10vh] bg-gray-500">
-               <div>
-                 <Input placeholder="Basic usage" />
-               </div>
-             footer input field
-
-           </div>
-         </div>
-       ) : (
-         <h1>select people to communicate with them</h1>
-       )
-      }
+     {!currentChat ? (
+        <div className="flex flex-col h-screen">
+          <div className="h-[10vh] bg-yellow-500">
+            top user data
+          </div>
+          <div className="h-[60vh] p-4">
+            center message
+            {messages.map((data, index) => {
+              return(
+              <div key={index} className={`flex ${data.receiverId === currentUserId ? 'justify-start' : 'justify-end'} my-2`}>
+                <div className={`p-2 rounded-lg max-w-xs ${data.receiverId === currentUserId ? 'bg-blue-300' : 'bg-gray-200'}`}>
+                  <h1>{data.message}</h1>
+                </div>
+              </div>
+              )
+              })}
+          </div>
+          <div className="h-[10vh] bg-gray-500 flex items-center p-4">
+            <Input placeholder="Basic usage" className="w-full" />
+          </div>
+        </div>
+      ) : (
+        <h1>select people to communicate with them</h1>
+      )
+    }
     </>
-  )
-}
+    )
+    }
+
 
 export default Conversation
