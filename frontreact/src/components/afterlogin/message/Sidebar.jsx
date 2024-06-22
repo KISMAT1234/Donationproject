d4ebb9548f12fb6d4f984aa9407848b33f5943d1
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-22 17:43:12 +0545

    adding setcurrent state to handle selected messages

--- frontreact/src/components/afterlogin/message/Sidebar.jsx
+++ frontreact/src/components/afterlogin/message/Sidebar.jsx
@@ -3,28 +3,37 @@ import axiosUrl from "../../url/Axiosurl";
 
 
 
-const Sidebar = () => {
+const Sidebar = ({ setCurrentChat}) => {
   const[user, setUser] = useState([])
 
   useEffect(() => {
      axiosUrl.get('user').then((response)=>{
-      console.log(response.data.data);
+      // console.log(response.data.data);
       setUser(response.data.data);
+     }).catch((error)=>{
+      console.log(error);
      }) 
   },[])
 
-  const userClick = () => {
-    console.log('clicked');
+  const userClick = (id) => {
+    console.log(id,'clicked');
+    axiosUrl.get(`user/${id}`).then((response)=>{
+      console.log(response.data.data.user,'specific id user');
+      setCurrentChat(response.data.data.user);
+     }).catch((error)=>{
+      console.log(error);
+     })
+
   }
     return (
     <>
       <div>Sidebar</div>
       <div>
          {
-          user.map((userList)=>{
+          user.map((userList,index)=>{
             return(
               <>
-                <button onClick={userClick()} className="mx-2 my-4 px-2 py-2  flex justify-between hover:bg-gray-300 ">
+                <button onClick={()=>userClick(userList._id)} className="mx-2 my-4 px-2 py-2  flex justify-between hover:bg-gray-300 ">
                   <div className="flex md:text-4xl">
                     <h1>Image</h1>
                     <h1 className="mx-2 text-xl font-light">{userList.username}</h1>
