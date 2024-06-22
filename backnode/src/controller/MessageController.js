d36dbd9f6a6033756a549d74ad3cf46987a5f9ef
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-22 17:30:08 +0545

    fetching message through id

--- backnode/src/controller/MessageController.js
+++ backnode/src/controller/MessageController.js
@@ -5,7 +5,7 @@ const responseInstance = new Handler();
 class MessageController {
     async sendMessage(req, res) {
         const newMessage = new Message(req.body);
-
+        console.log(newMessage);
         try {
           const savedMessage = await newMessage.save();
           return responseInstance.responseHandler(res,200,'Message saved successfully');
@@ -18,17 +18,29 @@ class MessageController {
 
     async fetchMessage(req,res){
         try{
-            const messageId = req.params.messageId
-            const messages = await Message.find()
+            const userId = req.user.userId
+            const messageId = req.params.id
+            console.log(messageId,'params id')
+
+            const [senderMessages, receiverMessages] = await Promise.all([
+                Message.find({ senderId: userId, receiverId: messageId }),
+                Message.find({ senderId: messageId, receiverId: userId })
+              ]);
+          
+              // Combine both sender and receiver messages into one array
+              const messages = [ ...receiverMessages,...senderMessages];
+
+              messages.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
 
             if(!messages){
                return responseInstance.responseHandler(res,400,'Message not found');     
             }
-            return responseInstance.responseHandler(res,200,'Message send Successfully',messages);
+            return responseInstance.responseHandler(res,200,'Message send Successfully',{messages});
 
         }
         catch(err){
-
+          console.log(err)
+          return responseInstance.responseHandler(res,500,'Backend server error');     
         }
     }
 }
