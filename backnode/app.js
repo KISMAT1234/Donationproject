8788c04da9312d5f4976fbf7d88cdd7713b49816
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-23 18:07:26 +0545

    implementing swagger for documentation

--- backnode/app.js
+++ backnode/app.js
@@ -6,6 +6,27 @@ import morgan from 'morgan';
 import errorHandler from './src/middleware/errorHandler.js';
 import dotenv from 'dotenv';
 import initializeSocket from './socket.js';
+import swaggerJSDoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+// import docs from './documentation/index.js'
+
+const options = {
+  definition:{
+    openapi: '3.0.0',
+    info: {
+      title: 'Fund Raising Platform API',
+      description:
+        'Welcome to our Blog Platform Backend Documentation. This guide offers a quick overview of the essential backend features that power our blogging platform, facilitating seamless integration for developers.<br><br>Our platform provides a secure authentication process, enabling users to register and log in using their email or social media credentials.<br><br>Leverage our robust text editor API to effortlessly create and store blogs.<br><br> This API supports rich content including text, images, and videos.The backend seamlessly manages media uploads, ensuring the safe storage and retrieval of images.<br><br>[Readme](https://readme.com)<br>[Github](https://github.com/mervixdev/expressServer)',
+      version: '0.1.9',
+    },
+    servers:[
+      {
+        api:'http://localhost:8000'
+      }
+    ]
+  },
+  apis:['./app.js']
+}
 
 dotenv.config();
 
@@ -28,6 +49,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Your other middleware and routes here
 app.use(mainRouter);
+
+const docs = swaggerJSDoc(options);
+app.use('/documentation', swaggerUi.serve, swaggerUi.setup(docs))
 
 app.get('/', (req, res) => {
   res.send('Hello world');
