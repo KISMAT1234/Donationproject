2a4ce3b8b9ee05d9f2524f199c75c6fdf6059538
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-23 13:47:07 +0545

    message send successfully

--- backnode/src/controller/MessageController.js
+++ backnode/src/controller/MessageController.js
@@ -1,14 +1,39 @@
+import mongoose from  'mongoose';
 import Message from "../model/Message.js";
 import Handler from "../logger/ResponseHandler.js"
+import Conversation from '../model/Conversation.js';
 const responseInstance = new Handler();
+const ObjectId = mongoose.Types.ObjectId;
 
 class MessageController {
     async sendMessage(req, res) {
-        const newMessage = new Message(req.body);
-        console.log(newMessage);
-        try {
-          const savedMessage = await newMessage.save();
-          return responseInstance.responseHandler(res,200,'Message saved successfully');
+      try {
+        const newMessage = req.body;
+        console.log(newMessage,'message rec');
+
+        let conversation = await Conversation.findOne({
+          participats: {$all: [newMessage.senderId, newMessage.receiverId]}
+        })
+        console.log(conversation,'conversation')
+
+        if(!conversation){
+          conversation = await Conversation.create({
+            participants: [newMessage.senderId, newMessage.receiverId]
+          })
+        }
+        console.log(conversation,'after save conversation')
+
+        
+          const saveMessage = new Message({...newMessage}); 
+          console.log(saveMessage)
+
+          if(saveMessage){
+             conversation.messages.push(saveMessage._id)
+          }
+          console.log(saveMessage,'push after save conversation')
+          console.log(conversation,'final push of id')
+          // await Promise.all([conversation.save(),saveMessage.save()]);
+          // return responseInstance.responseHandler(res,200,'Message saved successfully');
 
         } catch (err) {
             return responseInstance.responseHandler(res,500,'Error in backend')
@@ -22,20 +47,28 @@ class MessageController {
             const messageId = req.params.id
             console.log(messageId,'params id')
 
-            const [senderMessages, receiverMessages] = await Promise.all([
-                Message.find({ senderId: userId, receiverId: messageId }),
-                Message.find({ senderId: messageId, receiverId: userId })
-              ]);
+            let conversation = await Conversation.findOne({
+              participats: {$all: [userId, messageId]}
+            }).populate("messages")
+
+            // if (!ObjectId.isValid(userId) || !ObjectId.isValid(messageId)) {
+            //   return responseInstance.responseHandler(res, 400, 'Invalid ID format');
+            // }
+
+            // const [senderMessages, receiverMessages] = await Promise.all([
+            //     Message.find({ senderId: userId, receiverId: messageId }),
+            //     Message.find({ senderId: messageId, receiverId: userId })
+            //   ]);
           
-              // Combine both sender and receiver messages into one array
-              const messages = [ ...receiverMessages,...senderMessages];
+            //   // Combine both sender and receiver messages into one array
+            //   const messages = [ ...receiverMessages,...senderMessages];
 
-              messages.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+            //   messages.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
 
-            if(!messages){
-               return responseInstance.responseHandler(res,400,'Message not found');     
-            }
-            return responseInstance.responseHandler(res,200,'Message send Successfully',{messages});
+            // if(!messages){
+            //    return responseInstance.responseHandler(res,400,'Message not found');     
+            // }
+            // return responseInstance.responseHandler(res,200,'Message send Successfully',{messages});
 
         }
         catch(err){
