efd785ea835e945356f48f066acfe8b663a0b659
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-24 14:43:19 +0545

    minor change

--- frontreact/src/components/afterlogin/message/Messages.jsx
+++ frontreact/src/components/afterlogin/message/Messages.jsx
@@ -6,7 +6,7 @@ const Messages = () => {
   return (
     <>
         {/* <div>Messages</div> */}
-      <div className=" px-10 py-10">
+      <div className=" px-10 py-5">
         {/* <div className="w-[25%] h-[80vh] bg-red-500">
           <Sidebar/>
         </div> */}
