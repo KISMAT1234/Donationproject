1867885e808e2b0a9f6845f6e9b1a1be4ed90f26
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-27 20:34:48 +0545

    minor change

--- backnode/test/test.js
+++ backnode/test/test.js
@@ -1,24 +1,24 @@
-import chai from 'chai';
-import chaiHttp from 'chai-http';
-import { app } from '../app.js';
+// import chai from 'chai';
+// import chaiHttp from 'chai-http';
+// import { app } from '../app.js';
 
-chai.should();
-chai.use(chaiHttp);
+// chai.should();
+// chai.use(chaiHttp);
 
-describe('App unit testing', () => {
-  describe('Get Type unit Testing', () => {
-    it('should return 200', (done) => {
-      chai.request(app)
-        .get('/user')
-        .end((err, res) => {
-          if (err) {
-            console.log(err);
-          } else {
-            res.should.have.status(200);
-            res.body.should.be.a('array');
-          }
-          done();
-        });
-    });
-  });
-});
+// describe('App unit testing', () => {
+//   describe('Get Type unit Testing', () => {
+//     it('should return 200', (done) => {
+//       chai.request(app)
+//         .get('/user')
+//         .end((err, res) => {
+//           if (err) {
+//             console.log(err);
+//           } else {
+//             res.should.have.status(200);
+//             res.body.should.be.a('array');
+//           }
+//           done();
+//         });
+//     });
+//   });
+// });
