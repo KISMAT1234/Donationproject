0a89da5352b3de3b927f58c7198ec587b8ec07d0
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-29 21:49:56 +0545

    fetching payment data using asyncthunk

--- frontreact/src/slices/paymentSlice.jsx
+++ frontreact/src/slices/paymentSlice.jsx
@@ -1,4 +1,5 @@
 import axiosUrl from "../components/url/Axiosurl";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export const fetchPayment = createAsyncThunk("paymentInformation",async()=>{
     const response = await axiosUrl.get("/response");
@@ -6,5 +7,27 @@ export const fetchPayment = createAsyncThunk("paymentInformation",async()=>{
 })
 
 const paymentSlice = createSlice({
-    
+     name:"payment",
+     initialState:{
+        isLoading:false,
+        data:null,
+        isError:false
+     },
+     reducers:{},
+     extraReducers:(builder) => {
+        builder.addCase( fetchPayment.pending, (state,action)=>{
+             state.isLoading = true;
+        });
+        builder.addCase( fetchPayment.fulfilled, (state,action)=>{
+             state.isLoading = false;
+             state.data = action.payload;
+        });
+        builder.addCase( fetchPayment.fulfilled, (state,action)=>{
+            console.log("error",action.payload)
+            state.isError = true
+       });
+        
+     }
 })
+
+export default paymentSlice.reducer
