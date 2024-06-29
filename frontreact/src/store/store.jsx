b3346e04b8e0af6d23a22730bc11672cf96f7b3a
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-29 21:50:21 +0545

    configuring payment store globally

--- frontreact/src/store/store.jsx
+++ frontreact/src/store/store.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import Star from '../slices/addSlice'
+import paymentReducer from '../slices/paymentSlice'
 
 export const store = configureStore({
   reducer: {
     data:Star,
+    payment: paymentReducer,
   },
 })
