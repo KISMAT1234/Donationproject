16866c2f53dc5d6bc3a30740a919e2ba9674740d
KISMAT1234 <kismatbohora123@gmail.com>
2024-07-01 13:07:05 +0545

    tanstack query configured globally

--- frontreact/src/main.jsx
+++ frontreact/src/main.jsx
@@ -6,38 +6,13 @@ import "./index.css"
 import { store } from './store/store'
 import { Provider } from 'react-redux'
 import { socket } from './socket.jsx'
-// import { io } from 'socket.io-client'
 
-// const token = localStorage.getItem('token');
-// let userId;
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
-// if (token) {
-//   const decodedToken = jwtDecode(token);
-//   userId = decodedToken.id;
-// }
-
-
-
-// export const socket = io(import.meta.env.BackendServer).emit('join', 'userId')
-// export const socket = io("http://localhost:8000", {
-//     query: { userId },
-//     withCredentials: true,
-// });
-
-// export const socket = io("http://localhost:8000",{
-//   query: { userId }
-// })
-// // socket.emit('join', userId);
-// // console.log(socket,'socket url')
-
-
-// socket.on('connection', () => {
-//   console.log('frontend connection successfully') // x8WIv7-mJelg7on_ALbx
-// })
-
-// socket.on('disconnect', () => {
-//   console.log('frontend connection disconnect') // undefined
-// })
+const queryClient = new QueryClient()
 
 
   
@@ -47,7 +22,9 @@ import { socket } from './socket.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <Provider store={store}>
-          <App socket={socket}/>
+          <QueryClientProvider client={queryClient}>
+            <App socket={socket}/>
+          </QueryClientProvider>
         </Provider>
      </BrowserRouter>
 )
