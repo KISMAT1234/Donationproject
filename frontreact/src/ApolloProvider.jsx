6aa7f80eff34a65f7702815bcd24d5e85385867f
KISMAT1234 <kismatbohora123@gmail.com>
2024-07-07 16:31:27 +0545

    localhost server configured

---
+++ frontreact/src/ApolloProvider.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider as Provider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8000/graphql', // Replace with your GraphQL server URI
+  cache: new InMemoryCache(),
+});
+
+const ApolloProvider = ({ children }) => (
+  <Provider client={client}>
+    {children}
+  </Provider>
+);
+
+export default ApolloProvider;
