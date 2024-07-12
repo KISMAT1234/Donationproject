b958be76d27b7eb2b8ff5b02c5612366c16f20cd
KISMAT1234 <kismatbohora123@gmail.com>
2024-07-12 18:24:05 +0545

    fetched data

--- backnode/src/graphql/Resolvers/userResolver.js
+++ backnode/src/graphql/Resolvers/userResolver.js
@@ -1,15 +1,18 @@
 import User from "../../model/Userprofile.js";
-import Handler from "../logger/ResponseHandler.js"
+import Handler from "../../logger/ResponseHandler.js";
 const responseInstance = new Handler();
 
 export default {
     Query: {
         users: async () => {
             try{
+               const userData =  await User.find()
+            //    return responseInstance.responseHandler(res,500,'data fetch successfull',userData)
+               return userData
 
             }catch(err){
             console.log(err)
-            return responseInstance.responseHandler(res,500,'Backend server error')
+            // return responseInstance.responseHandler(res,500,'Backend server error')
 
         }
         }
