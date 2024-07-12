42616c6042d58d504da60b57811ce1a5e4ca43bf
KISMAT1234 <kismatbohora123@gmail.com>
2024-07-12 17:18:33 +0545

    try catch handled

--- backnode/src/graphql/Resolvers/userResolver.js
+++ backnode/src/graphql/Resolvers/userResolver.js
@@ -1,7 +1,17 @@
 import User from "../../model/Userprofile.js";
+import Handler from "../logger/ResponseHandler.js"
+const responseInstance = new Handler();
 
 export default {
     Query: {
-        users: async () => await User.find()
+        users: async () => {
+            try{
+
+            }catch(err){
+            console.log(err)
+            return responseInstance.responseHandler(res,500,'Backend server error')
+
+        }
+        }
     },
 };
