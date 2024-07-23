bc3c268ccc6e991879599e55ad56e44d12e589a2
KISMAT1234 <kismatbohora123@gmail.com>
2024-07-23 14:20:27 +0545

    main file for swagger where server is configured

--- backnode/documentation/index.js
+++ backnode/documentation/index.js
@@ -1,15 +1,35 @@
-import basicinfo from './basicinfo';
-import servers from './servers';
-import tags from './tags';
-import components from './components';
-import path from './path';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import path from 'path';
+import { userApi } from './user.js';
 
-const config = {
-  ...basicinfo,
-  ...servers,
-  ...tags,
-  ...components,
-  ...path,
+// Swagger definition
+const swaggerDefinition = {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Documentation',
+      version: '1.0.0',
+      description: 'API documentation for the application',
+    },
+    servers: [
+      {
+        url: 'http://localhost:8000',
+        description: 'Development server',
+      },
+    ],
+  apis: ['../src/routes/*.js'],
 };
 
-export default config;
+// Merge user and product API specs
+const swaggerSpec = {
+  ...swaggerDefinition,
+  paths: {
+    ...userApi.paths,
+  },
+};
+
+export const setupSwagger = (app) => {
+  // Serve Swagger API documentation
+  app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
+
