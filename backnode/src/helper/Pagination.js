4202918348bb65ca85fb810fc6cb9acec1c9c17a
KISMAT1234 <kismatbohora123@gmail.com>
2024-08-03 17:16:16 +0545

    checked pagination value

--- backnode/src/helper/Pagination.js
+++ backnode/src/helper/Pagination.js
@@ -1,6 +1,6 @@
 const Pagination = (req) =>{
     let page =Number(req.query.page) || 1;
-    console.log(page,'page value')
+    // console.log(page,'page value')
     let size = Number(req.query.size) || 3;
         // console.log(skip,'skip value');
     let skip = (page-1) * size;
