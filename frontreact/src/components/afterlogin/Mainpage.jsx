2e16085db60cc631bcb8945dd2f743fab7a69db8
KISMAT1234 <kismatbohora123@gmail.com>
2024-08-09 12:19:08 +0545

    horizontal scrollable added successfully

--- frontreact/src/components/afterlogin/Mainpage.jsx
+++ frontreact/src/components/afterlogin/Mainpage.jsx
@@ -153,7 +153,7 @@ function Content() {
 
 
   const more = (
-    <div className=" mx-10 block">
+    <div className=" mx-20 block ">
       <Link to="" className="flex hover:bg-gray-200 rounded-xl px-2 py-2">
           <CiFlag1 className="text-2xl mx-2 mt-2"/>
           <h1 className="text-xl hover:text-red-600 font-mono my-1">Report</h1>
@@ -198,50 +198,56 @@ function Content() {
 
   return (
     <>
-        <div className="" >
-          <div className="mx-5 flex fixed z-10 top-14 md:justify-between md:top-20 px-2 py-2 md:w-[72%] w-[94%] rounded-2xl  bg-stone-100 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
-              <Link to="/Mainpage">
-              <div className="ml-1 my-1 px-1 py-1 rounded-2xl bg-green-400 hover:bg-indigo-600 hover:text-white border-2 border-indigo-400 hover:border-indigo-800 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
-                <h1 className="text-sm md:text-xl">All</h1>
-              </div>
-              </Link>
-      
-              <Link to = "/Mainpage/search?category=Accident">
-              <div className="ml-1 my-1 px-1 py-1 rounded-2xl bg-green-400 hover:bg-indigo-600 hover:text-white border-2 border-indigo-400 hover:border-indigo-800 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
-                <h1 className="text-sm md:text-xl">Accident</h1>
-              </div>
-              </Link>
-      
-                <Link to ="">
-              <div className="ml-1 my-1 px-1 py-1 rounded-2xl bg-green-400 hover:bg-indigo-600 hover:text-white border-2 border-indigo-400 hover:border-indigo-800 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
-                <h1 className="text-sm md:text-xl">Disease</h1>
-              </div>
-              </Link>
-        
-              <Link to ="">
-              <div className="ml-1 my-1 px-1 py-1 rounded-2xl bg-green-400 hover:bg-indigo-600 hover:text-white border-2 border-indigo-400 hover:border-indigo-800 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
-                <h1 className="text-sm md:text-xl">Animal</h1>
-              </div>
-              </Link>
-        
-              <Link to ="">
-              <div className="ml-1 my-1 px-1 py-1 rounded-2xl bg-green-400 hover:bg-indigo-600 hover:text-white border-2 border-indigo-400 hover:border-indigo-800 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
-                <h1 className="text-sm md:text-xl">Environment</h1>
-              </div>
-              </Link>
-        
-              <Link to ="/Mainpage/search?category=Orphange">
-              <div className="ml-1 my-1 px-1 py-1 rounded-2xl bg-green-400 hover:bg-indigo-600 hover:text-white border-2 border-indigo-400 hover:border-indigo-800 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
-                <h1 className="text-sm md:text-xl">Orphange</h1>
-              </div>
-              </Link>
+    <div>
+    <div className="relative w-full ml-5">
+      <div className="overflow-x-auto whitespace-nowrap no-scrollbar fixed z-10 top-14 md:top-20 px-2 py-2 md:w-[75%] w-[94%] rounded-2xl bg-stone-100 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
+        <div className="inline-flex space-x-2">
+          <div className="flex-shrink-0 ml-1 my-1 px-4 py-2 rounded-2xl bg-green-400 hover:bg-indigo-600 hover:text-white border-2 border-indigo-400 hover:border-indigo-800 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
+            <Link to="/Mainpage">
+              <h1 className="text-xl">All</h1>
+            </Link>
+          </div>
 
-              <Link to ="">
-              <div className="ml-1 my-1 px-1 py-1 rounded-2xl bg-green-400 hover:bg-indigo-600 hover:text-white border-2 border-indigo-400 hover:border-indigo-800 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
-                <h1 className="text-sm md:text-xl">Other</h1>
-              </div>
-              </Link>
-            </div>
+          <div className="flex-shrink-0 ml-1 my-1 px-4 py-2 rounded-2xl bg-green-400 hover:bg-indigo-600 hover:text-white border-2 border-indigo-400 hover:border-indigo-800 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
+            <Link to="/Mainpage/search?category=Accident">
+              <h1 className="text-xl">Accident</h1>
+            </Link>
+          </div>
+
+          <div className="flex-shrink-0 ml-1 my-1 px-4 py-2 rounded-2xl bg-green-400 hover:bg-indigo-600 hover:text-white border-2 border-indigo-400 hover:border-indigo-800 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
+            <Link to="">
+              <h1 className="text-xl">Disease</h1>
+            </Link>
+          </div>
+
+          <div className="flex-shrink-0 ml-1 my-1 px-4 py-2 rounded-2xl bg-green-400 hover:bg-indigo-600 hover:text-white border-2 border-indigo-400 hover:border-indigo-800 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
+            <Link to="">
+              <h1 className="text-xl">Animal</h1>
+            </Link>
+          </div>
+
+          <div className="flex-shrink-0 ml-1 my-1 px-4 py-2 rounded-2xl bg-green-400 hover:bg-indigo-600 hover:text-white border-2 border-indigo-400 hover:border-indigo-800 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
+            <Link to="">
+              <h1 className="text-xl">Environment</h1>
+            </Link>
+          </div>
+
+          <div className="flex-shrink-0 ml-1 my-1 px-4 py-2 rounded-2xl bg-green-400 hover:bg-indigo-600 hover:text-white border-2 border-indigo-400 hover:border-indigo-800 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
+            <Link to="/Mainpage/search?category=Orphange">
+              <h1 className="text-xl">Orphange</h1>
+            </Link>
+          </div>
+
+          <div className="flex-shrink-0 ml-1 my-1 px-4 py-2 rounded-2xl bg-green-400 hover:bg-indigo-600 hover:text-white border-2 border-indigo-400 hover:border-indigo-800 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
+            <Link to="">
+              <h1 className="text-xl">Other</h1>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
             <InfiniteScroll 
               loadMore={()=>{
                 if(!isFetching){
@@ -251,7 +257,7 @@ function Content() {
               // loadMore={fetchNextPage()}
               hasMore={hasNextPage}
             >
-              <div className="mt-28 md:mt-20 ">
+              <div className="mt-36 md:mt-20 ">
                 {posts.map((data, index) => (
                   <div key={index} className="md:w-[90%]  px-5 py-5 mx-10 my-10 rounded-xl bg-slate-200 border-2 hover:border-green-500 border-gray-200  shadow-md  hover:shadow-lg transform hover:scale-105  transition duration-300 ease-in-out">
                     <div className="flex justify-between">
