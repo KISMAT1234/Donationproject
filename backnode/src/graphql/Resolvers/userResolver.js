7ee86842c77ca125cd8f9c535aa811ca13cdc3d6
KISMAT1234 <kismatbohora123@gmail.com>
2024-08-09 13:01:40 +0545

    email send to user after details save in db

--- backnode/src/graphql/Resolvers/userResolver.js
+++ backnode/src/graphql/Resolvers/userResolver.js
@@ -21,17 +21,66 @@ export default {
     Mutation: {
       Register: async (_, { data }) => {
         console.log(data,'data')
-        const { username, email, password, image } = data;
-        const user = new User({ username, email, password, image });
+        const { username, email, password } = data;
+        const user = new User({ username, email, password });
         user.slug = slugify(user.username, { lower: true });
-      
         try {
           await user.save();  // Save user to the database
-          return user;
-        } catch (error) {
-          console.error('Error saving user:', error);
-          throw new Error('Failed to save user');
+          const email = user.email
+          const userId = user._id;
+          if(user){
+              let value = emailToken.token({
+                  email,
+                  userId,
+                  reason:'verify',
+                  title:'Verify Account',
+                  subject:'Link to verify your account',
+                  info:user,
+                  template:'signupMessage'
+              })
+          }else{
+              return responseInstance.responseHandler(res,501,'User not found')
+          }
+          return responseInstance.responseHandler(res,200,'Email sent to your account for verification')
+        }
+        catch(err){
+             return res.status(500).json(err);
+             next(err);
         }
       }
     }
 };
+
+    // async store(req,res,next){
+    //     try{
+    //         // const value = await signupValidate(req.body)
+    //         let imageName="";
+    //         if(req.file){
+    //             imageName= req.file.filename;
+    //         }
+    //             const user = new User({...req.body, image:imageName}); 
+    //             user.slug = slugify(user.username, { lower: true });
+    //             console.log(user);
+    //             await user.save();
+    //             const email = user.email
+    //             const userId = user._id;
+    //             if(user){
+    //                 let value = emailToken.token({
+    //                     email,
+    //                     userId,
+    //                     reason:'verify',
+    //                     title:'Verify Account',
+    //                     subject:'Link to verify your account',
+    //                     info:user,
+    //                     template:'signupMessage'
+    //                 })
+    //             }else{
+    //                 return responseInstance.responseHandler(res,501,'User not found')
+    //             }
+    //             return responseInstance.responseHandler(res,200,'Email sent to your account for verification')
+    //         }
+    //         catch(err){
+    //              return res.status(500).json(err);
+    //              next(err);
+    //         }
+    // }
