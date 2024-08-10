d5cee25a38b99f7f1c2ce4468c69e0c474257c62
KISMAT1234 <kismatbohora123@gmail.com>
2024-08-10 12:01:40 +0545

    shutdow of http connection implemented

--- backnode/index.js
+++ backnode/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import { app, io } from './app.js';
 import Database from './src/connection/databaseconn.js'
+import mongoose from'mongoose';
 
 dotenv.config();
 Database();
@@ -15,9 +16,26 @@ const server = app.listen(port, () => {
 
 io.attach(server); 
 
-export const add = (a,b) => {
-  return a + b
-}
+process.on('SIGTERM', () => {
+  console.log('SIGTERM signal received: closing HTTP server');
+  server.close(() => {
+    console.log('HTTP server closed');
+    mongoose.connection.close(false, () => {
+      console.log('MongoDB connection closed');
+      process.exit(0);
+    });
+  });
+});
+
+
+
+
+
+
+
+// export const add = (a,b) => {
+//   return a + b
+// }
 
 // module.exports = { add }
 
