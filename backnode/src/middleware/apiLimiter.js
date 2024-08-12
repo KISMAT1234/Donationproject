bc5c9dd6de90b31d34e29365a36ba2eba70ec88a
KISMAT1234 <kismatbohora123@gmail.com>
2024-08-12 17:27:52 +0545

    [feat]: api rate limition implement for user to prevent from accesisve request

---
+++ backnode/src/middleware/apiLimiter.js
@@ -0,0 +1,10 @@
+import rateLimit from 'express-rate-limit';
+const apiRateLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 20, // Limit each IP to 100 requests per windowMs
+    message: 'Too many requests from this IP, please try again later.',
+    standardHeaders: true, // Return rate limit info in `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+export default apiRateLimiter;
