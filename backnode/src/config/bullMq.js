ddf6244b8582f14955ae545c398ef5561abf128d
KISMAT1234 <kismatbohora123@gmail.com>
2024-08-13 14:49:59 +0545

    added more priority config file

--- backnode/src/config/bullMq.js
+++ backnode/src/config/bullMq.js
@@ -1,14 +1,16 @@
 export const defaultQueueConfig = {
-    delay: 5000,
-    removeOnComplete: {
-        count: 100,
-        age: 60 * 60 * 24,
-    },
+    delay: 3000,
     attempts : 3,
     backoff : {
         type: "exponential",
         delay: 1000,
         maxDelay: 60 * 60 * 1000,
         factor: 2,
-    }
+    },
+    lifo: true,
+    priority: 2,
+    removeOnComplete:true,
+    removeOnFail: true,
+    timeout: 30000,
+    dependsOn: ['job1', 'job2']
 }
