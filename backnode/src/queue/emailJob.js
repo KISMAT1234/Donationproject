b3ba4a17a142c352da0c6a8f1d4506bdb49ed51b
KISMAT1234 <kismatbohora123@gmail.com>
2024-08-14 14:42:36 +0545

    shifting inside seperate folder in queue

---
+++ backnode/src/queue/emailJob.js
@@ -0,0 +1,48 @@
+import { Queue, Worker } from 'bullmq';
+import {client} from '../config/redis.js'; // Ensure this is ioredis
+import { defaultQueueConfig } from '../config/bullMq.js';
+import emailToken from '../helper/emailToken.js';
+import Handler from "../logger/ResponseHandler.js"
+const responseInstance = new Handler();
+
+export const emailQueueName = 'email-queue';
+
+export const emailQueue = new Queue(emailQueueName, {
+  connection: client,
+  defaultJobOptions: defaultQueueConfig,
+});
+
+export const handler = new Worker(
+  emailQueueName,
+  async (job) => {
+    
+    const data = job.data;
+    data?.map( (user) => {
+      const email = user.email
+      const userId = user._id;
+      if(user){
+          let value = emailToken.token({
+              email,
+              userId,
+              reason:'verify',
+              title:'Verify Account',
+              subject:'Link to verify your account',
+              info:user,
+              template:'signupMessage'
+          })
+      }else{
+          return responseInstance.responseHandler(res,501,'User not found')
+      }
+    });
+  },
+  { connection: client }
+);
+
+
+handler.on('completed', () => {
+  console.log('Handler job completed');
+});
+
+handler.on('failed', (err) => {
+  console.error('Handler job failed:', err);
+});
