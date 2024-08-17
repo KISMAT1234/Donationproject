db3cd305cbfb43b46b5182cb50b1d73653608cd8
KISMAT1234 <kismatbohora123@gmail.com>
2024-08-17 14:28:44 +0545

    error handling with try and const

---
+++ frontreact/src/middleware/googleAuth.jsx
@@ -0,0 +1,10 @@
+import axiosUrl from "../components/url/Axiosurl"
+
+export const googleAuth = (code) => {
+    try{
+        console.log(code,'code send by google and going to send this code to backend')
+        axiosUrl.get(`/user/google?code=${code}`)
+    }catch(error){
+       console.log(error,'error at auth')
+    }
+}
