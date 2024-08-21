3805b59edd54e860dd273dde45e4906097427194
KISMAT1234 <kismatbohora123@gmail.com>
2024-08-21 18:54:23 +0545

    returning user response

--- frontreact/src/middleware/googleAuth.jsx
+++ frontreact/src/middleware/googleAuth.jsx
@@ -3,7 +3,7 @@ import axiosUrl from "../components/url/Axiosurl"
 export const googleAuth = (code) => {
     try{
         console.log(code,'code send by google and going to send this code to backend')
-        axiosUrl.get(`/user/google?code=${code}`)
+        return axiosUrl.get(`/user/google?code=${code}`)
     }catch(error){
        console.log(error,'error at auth')
     }
