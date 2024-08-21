1e3bc47a2b750c4833e21d266b04db23f10a66eb
KISMAT1234 <kismatbohora123@gmail.com>
2024-08-21 18:58:24 +0545

    time increased of limitng

--- backnode/src/middleware/apiLimiter.js
+++ backnode/src/middleware/apiLimiter.js
@@ -1,7 +1,7 @@
 import rateLimit from 'express-rate-limit';
 const apiRateLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 20, // Limit each IP to 100 requests per windowMs
+    max: 100, // Limit each IP to 100 requests per windowMs
     message: 'Too many requests from this IP, please try again later.',
     standardHeaders: true, // Return rate limit info in `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
